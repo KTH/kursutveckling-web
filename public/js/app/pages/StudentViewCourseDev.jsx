9ce84f350220e9b9889a86017251713a5b1e92c0
Elena Rakhimova <elenara@kth.se>
2019-04-23 16:22:21 +0200

    Refactored code and fixed correct collapsing

--- public/js/app/pages/StudentViewCourseDev.jsx
+++ public/js/app/pages/StudentViewCourseDev.jsx
@@ -1,67 +1,123 @@
 import React, { Component } from 'react'
 import { inject, observer} from 'mobx-react'
-import { Button, Alert, Card, CardText, CardBody,
-  CardTitle, CardFooter} from 'reactstrap'
+import { Collapse, Button, Alert, Card, CardText, CardBody,
+  CardTitle, CardFooter, Table} from 'reactstrap'
 import i18n from '../../../../i18n'
 
 import CourseTitle from '../components/CourseTitle.jsx'
 
-function GraphBlock ({text}) {
+const GrayTextBlock = ({header, text}) => {
   return (
-    <span className='textBlock' dangerouslySetInnerHTML={{__html: text}}>
+    <span>
+      <h4>{header}</h4>
+      <Table responsive>
+        <tbody>
+          <tr>
+            <td colSpan="6" dangerouslySetInnerHTML={{__html: text}}>                    
+            </td>
+          </tr>
+        </tbody>
+      </Table>  
     </span>
-    )
-}
-
-function GraphCollapse ({header, text, label}) {
-  return (
-    <div className='courseInfoDevCollapse'>
-      <div className='card collapsible'>
-        <div className='card-header primary' role='tab' id={'headingWhite' + label} tabindex='0'>
-          <h4 className='mb-0'>
-            <a className='collapse-header' data-toggle='collapse' href={'#collapseWhite' + label} aria-expanded='false' aria-controls={'collapseWhite' + label}>{header}</a>
-          </h4>
-        </div>
-        <div id={'collapseWhite' + label} className='collapse hide' role='tabpanel' aria-labelledby={'headingWhite' + label}>
-          <div className='card-body  col'>
-            <GraphBlock text={text} />
-          </div>
-        </div>
-      </div>
-    </div>
   )
 }
 
+class ProgramCollapse extends Component {
+  constructor(props) {
+    super(props)
+    this.toggleHeader = this.toggleHeader.bind(this)
+    this.state = {collapseProgram: false}
+  }
+  toggleHeader() {
+    this.setState(state => ({collapseProgram: !state.collapseProgram}))
+  }
+  render () {
+    const label = this.props.label
+    return (
+        <div className='card collapsible programs-list white' >
+          <span className='card-header' role='tab'  tabIndex='0' onClick={this.toggleHeader}>
+              <a className='collapse-header' id={'programHeading' + label} data-toggle='collapse' href={'#collapsePrograms' + label} aria-controls={'collapsePrograms' + label}>{this.props.header}</a>
+          </span>
+          <Collapse isOpen={this.state.collapseProgram} toggler={'#programHeading' + label}>
+            <div className='card-body  col'>
+              <span className='textBlock' dangerouslySetInnerHTML={{__html: this.props.text}}></span> 
+            </div>
+          </Collapse>
+        </div>
+    )
+  }
+}
+class TableForCourse extends Component {
+  constructor(props) {
+    super(props)
+    this.toggleRound = this.toggleRound.bind(this)
+    // this.toggle = this.toggle.bind(this)
+    this.state = {collapse: true}
+  }
+
+  toggleRound() {
+    this.setState(state => ({collapse: !state.collapse}))
+  }
+
+  render () {    
+    return(
+      <div className='card collapsible blue'>
+        <span className='table-title card-header'  role="tab" tabIndex='0' onClick={this.toggleRound}>
+            <a id={this.props.togglerId}  aria-expanded={this.state.collapse}>Kursomgång: {this.props.courseRound}</a> 
+        </span>
+        {/*  */}
+        <Collapse isOpen={this.state.collapse} toggler={'#'+this.props.togglerId}>
+          <ProgramCollapse header="Obligatorisk inom program" text="CDATE2, DCADF1 FSDF3" label={this.props.togglerId}/>
+          <span className="right-links" >
+            <a href='#' >KursPM: 2019-05-20</a> <a href='#' >Kursanalys: 2019-05-25</a>
+          </span>
+          <Table responsive>
+            <thead>
+                <tr>
+                    <th>Kursansvarig, Examinator</th>
+                    <th>Antal reg. studenter</th>
+                    <th>Form av examination och när den utförs</th>
+                    <th>Kommentar till examination</th>
+                    <th alt='; i % av aktiva (totalt) vid första ex-tillfället'>Examinationsgrad</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> 
+                        <p><b>Examinator</b></p>                  
+                        <p>Ulf Sellgren</p>
+                        <p><b>Kursansvarig</b></p>                  
+                        <p>Ellen Bergseth</p>
+                    </td>
+                    <td>72</td>
+                    <td>
+                        <p>Assignments (3hp) </p><p> 2016-06-07</p>
+                        <p>Project (3HP) </p><p> 2016-06-07</p>
+                    </td>
+                    <td> 
+                        <p>It consists of several ABCD tests</p>
+                    </td>
+                    <td>
+                        <p>84% (ÖVN1)</p>
+                        <p>46% (ÖVN2)</p>
+                    </td>
+                </tr>
+            </tbody>
+          </Table> 
+          <GrayTextBlock header="Förändringar som införs i årets kurs (exempel fr kursanalys)" text="<p>Mer mekanismmodellering och toleranssättning	</p><p>Mer materil och labs	</p><p>Mer mekanismmodellering och toleranssättning	</p><p>Mer materil och labs	</p>"/>
+          <GrayTextBlock header="Kommentar till ändringar" text="<p>Laddat upp kursanalys</p>"/>
+          <p className="underlined">Senaste ändring: 2019-05-25 20:54</p>
+        </Collapse>
+      </div>          
+    )}
+}
 @inject(['adminStore']) @observer
 class StudentViewCourseDev extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      lastChangedBy: this.props.adminStore.sellingTextAuthor,
-      enteredEditMode: true,
-      hasDoneSubmit: false,
-      isError: false,
       errMsg: ''
     }
-    this.doChangeText = this.doChangeText.bind(this)
-    this.doPreview = this.doPreview.bind(this)
-  }
-
-  doChangeText (event) {
-    event.preventDefault()
-    this.setState({
-      hasDoneSubmit: false,
-      enteredEditMode: true,
-      isError: false
-    })
-  }
-
-  doPreview (event) {
-    event.preventDefault()
-    this.setState({
-      enteredEditMode: false,
-      isError: false
-    })
   }
 
   render () {
@@ -82,124 +138,10 @@ class StudentViewCourseDev extends Component {
         {this.state.errMsg ? <Alert color='info'><p>{this.state.errMsg}</p></Alert> : ''}
 
         {/* ---IF in edit mode or preview mode--- */}
-        <div className='TextEditor--SellingInfo col'>
-          {/* ---TEXT Editors for each language--- */}
-          <p>{courseDevLabels.label_course_dev_info}</p>
-          <span className='row_name'><h3>SF1624 VT 2019 </h3><p>Published: 2019-05-30 | Kursanalys: 2019-05-25 | Last changed: 2019-06-01</p></span>
-          <span className='Editors--Area' key='editorsArea' role='tablist'>
-            <span className='left' key='leftEditorForSwedish'>
-              <Card className='KursInfo--Dev'>
-                <CardBody>
-                  <CardTitle>Snabbfakta</CardTitle>
-                  <CardText>
-                    <h4>Antal reg. studenter</h4>
-                    <p>72</p>
-                    <h4>Kursansvarig</h4>
-                    <p>Ulf Sellgren</p>
-                    <p>Ellen Bergseth</p>
-                    <h4></h4>
-                    <p></p>
-                  </CardText>
-                </CardBody>
-                <CardFooter className='text-right'>
-                  <Button color='primary'>Ladda ner kursanalys</Button>
-                </CardFooter>
-              </Card>
-              </span>
-              <span className='right' key='leftEditorForSwedish'>
-              <Card >
-                <CardBody>
-                  <CardTitle>Examinationer</CardTitle>
-                  <CardText>
-                    <h4>Form av examination och när den utförs</h4>
-                    <p>Assignments (3hp) 2016-06-07</p>
-                    <p>Project (3HP) 2016-06-07</p>
-                    <h4>Examinationsgrad; i % av aktiva (totalt) vid första ex-tillfället.</h4>
-                    <p>84% (ÖVN1)</p>
-                    <p>46% (ÖVN2)</p>
-                  </CardText>
-                </CardBody>
-                <CardFooter className='text-right'>
-                  <h4>Kommentar till examination</h4>
-                  <p>Hasdjhasd asdiodhfad refakfjdv dvfgerjhqrjf</p>
-                </CardFooter>
-              </Card>
-            </span>
-            <span className='right' key='rightEditorForEnglish'>
-              <Card className='KursInfo--Dev'>
-                <CardBody>
-                  <CardTitle>Förändringar som införs i årets kurs (exempel fr kursanalys)</CardTitle>
-                  <CardText>
-                    <p>Mer mekanismmodellering och toleranssättning	</p>
-                    <p>Mer materil och labs	</p>
-                  </CardText>
-                </CardBody>
-                <CardFooter className='text-right'>
-                  <h4>Kommentar till anndrigar</h4>
-                  <p>Hasdjhasd asdiodhfad refakfjdv dvfgerjhqrjf</p>
-                </CardFooter>
-              </Card>
-            </span>
-          </span>
-          <span className='row_name'><h3>SF1624 HT 2018</h3> <p>Published: 2018-12-30 | Kursanalys: 2018-12-25 | Last changed: 2019-01-01</p></span>
-          <span className='Editors--Area' key='editorsArea' role='tablist'>
-            <span className='left' key='leftEditorForSwedish'>
-              <Card className='KursInfo--Dev'>
-                <CardBody>
-                  {/* <CardTitle>Snabbfakta</CardTitle> */}
-                  <CardText>
-                    <h4>Antal reg. studenter</h4>
-                    <p>72</p>
-                    <h4>Kursansvarig</h4>
-                    <p>Ulf Sellgren</p>
-                    <p>Ellen Bergseth</p>
-                    <h4></h4>
-                    <p></p>
-                  </CardText>
-                </CardBody>
-                <CardFooter className='text-right'>
-                  <Button onClick={this.doStartSellingText} color='primary'>Ladda ner kursanalys</Button>
-                </CardFooter>
-              </Card>
-              </span>
-              <span className='right' key='leftEditorForSwedish'>
-              <Card >
-                <CardBody>
-                  {/* <CardTitle>Examinationer</CardTitle> */}
-                  <CardText>
-                    <h4>Form av examination och när den utförs</h4>
-                    <p>Assignments (3hp)</p>
-                    <p>Project (3HP) </p>
-                    <p>2016-06-07</p>
-                    <h4>Examinationsgrad; i % av aktiva (totalt) vid första ex-tillfället.</h4>
-                    <p>84%</p>
-                  </CardText>
-                </CardBody>
-                <CardFooter className='text-right'>
-                  <h4>Kommentar till examination</h4>
-                  <p>Hasdjhasd asdiodhfad refakfjdv dvfgerjhqrjf</p>
-                </CardFooter>
-              </Card>
-            </span>
-            <span className='right' key='rightEditorForEnglish'>
-              <Card className='KursInfo--Dev'>
-                <CardBody>
-                  <CardTitle>Förändringar som införs i årets kurs (exempel fr kursanalys)</CardTitle>
-                  <CardText>
-                    <p>Mer mekanismmodellering och toleranssättning	</p>
-                    <p>Mer materil och labs	</p>
-                    <p>Mer mekanismmodellering och toleranssättning	</p>
-                    <p>Mer materil och labs	</p>
-                  </CardText>
-                </CardBody>
-                <CardFooter className='text-right'>
-                  <h4>Kommentar till anndrigar</h4>
-                  <p>Hasdjhasd asdiodhfad refakfjdv dvfgerjhqrjf</p>
-                </CardFooter>
-              </Card>
-            </span>
-          </span>
-          {/* PROVA TABELL VIEW */}
+        <div className='tables-list col'>
+          <h3>2018</h3>
+          <TableForCourse courseRound="HT 2018" togglerId="toggler1"/>
+          <TableForCourse courseRound="VT 2018" togglerId="toggler2"/>
         </div>
       </div>
     )
