7fc534af6c94d63551cee3a06289a69f24a22f6d
Elena Rakhimova <elenara@kth.se>
2019-05-14 15:48:28 +0200

    Rebuild collapse to show extra information

--- public/js/app/components/CollapseExtraInfo.jsx
+++ public/js/app/components/CollapseExtraInfo.jsx
@@ -1,44 +1,38 @@
 import React, { Component } from 'react'
-import { Collapse, Table } from 'reactstrap'
-
-const GrayTextBlock = ({header, text}) => {
-  return (
-        <Table responsive>
-          <tbody>
-            <tr>
-              <td colSpan='6' dangerouslySetInnerHTML={{__html: text}}>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-    )
-}
+import { Collapse } from 'reactstrap'
 
 class CollapseExtraInfo extends Component {
   constructor (props) {
     super(props)
     this.toggleHeader = this.toggleHeader.bind(this)
-    this.state = {collapseProgram: this.props.isOpen}
+    this.state = {collapseExtraInfo: this.props.isOpen}
   }
   toggleHeader () {
-    this.setState(state => ({collapseProgram: !state.collapseProgram}))
+    this.setState(state => ({collapseExtraInfo: !state.collapseExtraInfo}))
   }
   render () {
     const label = this.props.label
     return (
-        <div className='card collapsible programs-list white' >
-          <span className='card-header program-rubrik' role='tab' tabIndex='0' onClick={this.toggleHeader}>
-              <a className='collapse-header title' id={'programHeading' + label} aria-expanded={this.state.collapseProgram} load='false' data-toggle='collapse' href={'#collapsePrograms' + label} aria-controls={'collapsePrograms' + label}>{this.props.header}</a>
-          </span>
-          <Collapse color='white' isOpen={this.state.collapseProgram} toggler={'#programHeading' + label}>
-            <div className='card-body col summary'>
-              {this.props.grayTextBlock
-              ? <GrayTextBlock text={this.props.text} />
-              : <span className='textBlock' dangerouslySetInnerHTML={{__html: this.props.text}}></span>
-              }
-            </div>
-          </Collapse>
-        </div>
+      <div className='card collapsible programs-list white' >
+        <span className='card-header program-rubrik' role='tab' tabIndex='0' onClick={this.toggleHeader}>
+          <a className='collapse-header title' id={label} aria-expanded={this.state.collapseExtraInfo} load='false' data-toggle='collapse'>{this.props.header}</a>
+        </span>
+        <Collapse color='white' isOpen={this.state.collapseExtraInfo} toggler={label}>
+          <div className='card-body col summary'>
+            <h4>Kommentar till examination</h4>
+            <span className='textBlock' dangerouslySetInnerHTML={{__html: this.props.text}}></span>
+            <h4>Obligatorisk inom program</h4>
+            <span className='textBlock' dangerouslySetInnerHTML={{__html: this.props.text}}></span>
+            <h4>Kurstillfällen som ingår</h4>
+            <span className='textBlock' dangerouslySetInnerHTML={{__html: this.props.text}}></span>
+            <h4>Datum för publicerad kursanalys</h4>
+            <span className='textBlock' dangerouslySetInnerHTML={{__html: this.props.text}}></span>
+            <p>Publicerad första gången: 2019-05-23</p>
+            <p>Senaste ändrad: <i>ej ändrad efter publicering</i></p>
+            <p>Kommentar tillgjorda ändringar: -</p>
+          </div>
+        </Collapse>
+      </div>
       )
   }
   }
