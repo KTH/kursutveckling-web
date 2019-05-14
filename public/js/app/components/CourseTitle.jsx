54f0ae767302c690bf9f8bc04b457f2c81105b63
Elena Rakhimova <elenara@kth.se>
2019-05-14 11:34:23 +0200

    Added KIP mini navigation for om-kursen projects and reformated course title

--- public/js/app/components/CourseTitle.jsx
+++ public/js/app/components/CourseTitle.jsx
@@ -12,28 +12,29 @@ class CourseTitle extends Component {
     title.course_credits = title.apiError ? '' : title.course_credits !== EMPTY && title.course_credits.toString().indexOf('.') < 0 ? title.course_credits + '.0' : title.course_credits
     return (
       <div id='course-title' className='courseTitle col'>
-        {/* {title.apiError
-          ? <h1><span property='aiiso:code'>{title.course_code}</span>
+        <h1>{pageTitle}</h1>
+        {title.apiError
+          ? <h4><span property='aiiso:code'>{title.course_code}</span>
             <span property='teach:courseTitle'>
               <Alert color='info' aria-live='polite'>
                 {i18n.messages[langIndex].pageTitles.alertMessages.kopps_api_down}
               </Alert>
             </span>
-          </h1>
-          : <h1><span property='aiiso:code'>{title.course_code}</span>
+          </h4>
+          : <h4><span property='aiiso:code'>{title.course_code}</span>
             <span property='teach:courseTitle'> {title.course_title}</span>
             <span content={title.course_credits} datatype='xsd:decimal' property='teach:ects'>
               &nbsp;{this.props.language === 'en' ? title.course_credits : title.course_credits.toString().replace('.', ',')}&nbsp;{this.props.language === 'en' ? 'credits' : 'hp'}
             </span>
-          </h1>
-        } */}
-        <h1>{pageTitle}</h1>
+          </h4>
+        }
+{/*
         <h4><span property='aiiso:code'>{title.course_code}</span>
             <span property='teach:courseTitle'> {title.course_title}Calculus in Several Variable 7,5</span>
             <span content={title.course_credits} datatype='xsd:decimal' property='teach:ects'>
               &nbsp;{this.props.language === 'en' ? title.course_credits : title.course_credits.toString().replace('.', ',')}&nbsp;{this.props.language === 'en' ? 'credits' : 'hp'}
             </span>
-          </h4>
+          </h4> */}
       </div>
     )
   }
---
+++ public/js/app/components/KipNav.jsx
@@ -0,0 +1,33 @@
+import {Table} from 'reactstrap'
+import React from 'react'
+
+
+const KipLinkNav = ({courseCode, trans, lang}) => { // courseCode, lang, startCards
+  const kursOmLink = `/student/kurser/kurs/${courseCode}?l=${lang}`
+  const kursAdmin = `/admin/kurser/kurs/${courseCode}?l=${lang}`
+  return (
+    <span>
+      <Table className='kip-menu'>
+        <tbody>
+          <tr>
+            <td colSpan='2'>
+              <h4>Om kursen</h4>
+              <p>
+                {/* <a href={`/admin/kurser/kurs/edit/${courseCode}?l=${lang}`} alt={startCards.sellingText_btn} className='btn btn-primary'>{startCards.sellingText_btn}</a> */}
+                <a href={kursOmLink} alt='Tillbaka till Kursinformation vy'>Kursinformation</a>
+              </p>
+              <p>
+                <b>Kursens utveckling och historik</b>
+              </p>
+              <p>
+                <a href={kursAdmin} alt='Tillbaka till Kursens utveckling och historik vy'>Administrera</a>
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </span>
+    )
+}
+
+export default KipLinkNav
