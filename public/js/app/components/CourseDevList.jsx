72510c9f49db2c566df2d112c93d85bc51304f7c
Elena Rakhimova <elenara@kth.se>
2019-05-14 15:45:38 +0200

    Passing the whole object instead of just name

--- public/js/app/components/CourseDevList.jsx
+++ public/js/app/components/CourseDevList.jsx
@@ -1,15 +1,15 @@
 import TableForCourseRound from './TableForCourseRound'
 import React from 'react'
 
-const CourseDevPerRound = ({arrOfRoundsObj, year}) => {
+const CourseDevAllRounds = ({arrOfRoundObj, year}) => {
   return (
     <span>
       <h3>{year}</h3>
       {
-      arrOfRoundsObj.length === 0
+      arrOfRoundObj.length === 0
       ? <p>Kursutveckling saknas</p>
-      : arrOfRoundsObj.map(roundObj =>
-        <TableForCourseRound courseRound={roundObj.analysisName} togglerId='toggler1' />
+      : arrOfRoundObj.map(roundObj =>
+        <TableForCourseRound courseRoundObj={roundObj} togglerId={roundObj._id} />
       )
     }
     </span>
@@ -17,11 +17,12 @@ const CourseDevPerRound = ({arrOfRoundsObj, year}) => {
 }
 
 const CourseDevAllYears = ({allYearsObj}) => {
+  console.log('arrOfRoundObj', allYearsObj)
   const yearsDescending = Object.keys(allYearsObj).reverse()
   return (
     <div className='tables-list col'>
       {
-        yearsDescending.map(year => <CourseDevPerRound arrOfRoundsObj={allYearsObj[year]} year={year} />)
+        yearsDescending.map(year => <CourseDevAllRounds arrOfRoundObj={allYearsObj[year]} year={year} />)
       }
     </div>
   )
