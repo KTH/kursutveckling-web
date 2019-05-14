bb5403087673a4e52f770a7a6840ece5a1cd44b0
Elena Rakhimova <elenara@kth.se>
2019-05-14 14:31:41 +0200

    Code splitting to use components imported from other files

--- public/js/app/pages/StudentViewCourseDev.jsx
+++ public/js/app/pages/StudentViewCourseDev.jsx
@@ -1,153 +1,11 @@
 import React, { Component } from 'react'
 import { inject, observer} from 'mobx-react'
-import { Collapse, Button, Alert, Card, CardText, CardBody,
-  CardTitle, CardFooter, Table} from 'reactstrap'
+import { Alert } from 'reactstrap'
 import i18n from '../../../../i18n'
 
-import CourseTitle from '../components/CourseTitle.jsx'
-import KipLinkNav from '../components/KipNav.jsx'
-
-const GrayTextBlock = ({header, text}) => {
-  return (
-      <Table responsive>
-        <tbody>
-          <tr>
-            <td colSpan="6" dangerouslySetInnerHTML={{__html: text}}>                    
-            </td>
-          </tr>
-        </tbody>
-      </Table>  
-  )
-}
-
-class ProgramCollapse extends Component {
-  constructor(props) {
-    super(props)
-    this.toggleHeader = this.toggleHeader.bind(this)
-    this.state = {collapseProgram: this.props.isOpen}
-  }
-  toggleHeader() {
-    this.setState(state => ({collapseProgram: !state.collapseProgram}))
-  }
-  render () {
-    const label = this.props.label
-    return (
-      <div className='card collapsible programs-list white' >
-        <span className='card-header program-rubrik' role='tab'  tabIndex='0' onClick={this.toggleHeader}>
-            <a className='collapse-header title' id={'programHeading' + label} aria-expanded={this.state.collapseProgram} load='false' data-toggle='collapse' href={'#collapsePrograms' + label} aria-controls={'collapsePrograms' + label}>{this.props.header}</a>
-        </span>
-        <Collapse color="white" isOpen={this.state.collapseProgram} toggler={'#programHeading' + label}>
-          <div className='card-body col summary'>
-            {this.props.grayTextBlock ?
-              <GrayTextBlock text={this.props.text}/>
-            :
-              <span className='textBlock' dangerouslySetInnerHTML={{__html: this.props.text}}></span> 
-            }
-          </div>
-        </Collapse>
-      </div>
-    )
-  }
-}
-class TableForCourse extends Component {
-  constructor(props) {
-    super(props)
-    this.toggleRound = this.toggleRound.bind(this)
-    // this.toggle = this.toggle.bind(this)
-    this.state = {collapse: true}
-  }
-
-  toggleRound() {
-    this.setState(state => ({collapse: !state.collapse}))
-  }
-
-  render () {    
-    return (
-      <div className='card collapsible white blue'>
-        <span className='table-title card-header'  role="tab" tabIndex='0' onClick={this.toggleRound}>
-            <a id={this.props.togglerId}  aria-expanded={this.state.collapse} load='false'>Kursomgång: {this.props.courseRound}</a> 
-        </span>
-        {/*  */}
-        <Collapse isOpen={this.state.collapse} toggler={'#'+this.props.togglerId}>
-          <ProgramCollapse isOpen={false} header="Kurstillfällen som ingår" text="HT 2018 CMEDT1, HT 2018 CDEPR1 m.fl., HT 2018 CMATD1 m.fl." label={this.props.togglerId}/>
-          <ProgramCollapse isOpen={false} header="Obligatorisk inom program" text="CDATE2, DCADF1, FSDF3" label={this.props.togglerId}/>
-          <span className="right-links" >
-            <a href='#' >Kursplan: 2019-05-20</a>
-            <a href='#'>Kurs-PM: 2019-05-20</a> 
-            <a href='#' >Kursanalys: 2019-05-25</a>
-          </span>
-          <Table responsive>
-            <thead>
-                <tr>
-                    <th>Kursansvarig, Examinator</th>
-                    <th>Antal reg. studenter</th>
-                    <th>Form av examination</th>
-                    <th>Kommentar till examination</th>
-                    <th alt='; i % av aktiva (totalt) vid första ex-tillfället'>Examinationsgrad</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td> 
-                        <p><b>Examinator</b></p>                  
-                        <p>Elena Rakhimova</p>
-                        <p><b>Kursansvarig</b></p>                  
-                        <p>Jeanette Skog</p>
-                    </td>
-                    <td>72</td>
-                    <td>
-                        <p>Assignments (3hp) </p>
-                        <p>Project (3HP) </p>
-                    </td>
-                    <td> 
-                        <p>It consists of several ABCD tests</p>
-                    </td>
-                    <td>
-                        <p>84%</p>
-                      
-                    </td>
-                </tr>
-            </tbody>
-          </Table> 
-          {/* <GrayTextBlock header="Förändringar som införs i årets kurs (exempel fr kursanalys)" text="<p>Mer mekanismmodellering och toleranssättning	</p><p>Mer materil och labs	</p><p>Mer mekanismmodellering och toleranssättning	</p><p>Mer materil och labs	</p>"/> */}
-          {/* <GrayTextBlock header="Kommentar till ändringar" text="<p>Laddat upp kursanalys</p>"/> */}
-          <ProgramCollapse isOpen={true} grayTextBlock="true" header="Förändringar som införs i årets kurs (exempel fr kursanalys)" text="<p>Mer mekanismmodellering och toleranssättning	</p><p>Mer materil och labs	</p>" label={this.props.togglerId}/>
-          <ProgramCollapse isOpen={true} grayTextBlock="true" header="Kommentar till ändringar" text="<p>Laddat upp kursanalys</p>" label={this.props.togglerId}/>
-          <p className="underlined">Senaste ändring: 2019-05-25</p>
-        </Collapse>
-      </div>          
-    )}
-}
-
-const CourseDevByPerYear = ({allYearsObj}) => {
-  const yearsDescending = Object.keys(allYearsObj).reverse()
-  return (
-    <div className='tables-list col'>
-    { 
-      yearsDescending.map(year => <CourseDevPerRound arrOfRoundsObj={allYearsObj[year]} year={year}/>)
-    }
-    </div>
-  )
-}
-
-const CourseDevPerRound({arrOfRoundsObj, year}) => {
-  return (
-    <span>
-      <h3>{year}</h3>
-      {
-        arrOfRoundsObj.length === 0 ?
-          <p>Kursutveckling saknas</p>
-        :
-        arrOfRoundsObj.map(roundObj => 
-          <TableForCourse courseRound={roundObj.analysisName} togglerId="toggler1"/>
-        )
-
-      }
-    </span>
-  )
-}
-
-
+import CourseTitle from '../components/CourseTitle'
+import KipLinkNav from '../components/KipNav'
+import CourseDevAllYears from '../components/CourseDevList'
 
 @inject(['adminStore']) @observer
 class StudentViewCourseDev extends Component {
@@ -164,7 +22,6 @@ class StudentViewCourseDev extends Component {
     const lang = courseAdminData.lang === 'en' ? 0 : 1
     const courseCode = courseAdminData.courseTitleData.course_code
     const { pageTitles, courseDevLabels, startCards} = i18n.messages[lang]
-    console.log("asdewranalysisData", analysisData)
 
     return (
       <div key='kursinfo-container' className='kursinfo-main-page col' >
@@ -177,7 +34,7 @@ class StudentViewCourseDev extends Component {
         <KipLinkNav courseCode={courseCode} lang={courseAdminData.lang} trans={startCards} />
 
         {this.state.errMsg ? <Alert color='info'><p>{this.state.errMsg}</p></Alert> : ''}
-        <CourseDevByPerYear allYearsObj={analysisData}/>
+        <CourseDevAllYears allYearsObj={analysisData}/>
 {/* 
         <div className='tables-list col'>
           <h3>2019</h3>
