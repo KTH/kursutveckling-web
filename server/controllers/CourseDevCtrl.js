6f1bdee10278ed05cb8391438321fc9a30feabe6
Elena Rakhimova <elenara@kth.se>
2019-05-14 12:08:46 +0200

    Rebuild controller to communicate with kursutveckling api

--- server/controllers/CourseDevCtrl.js
+++ server/controllers/CourseDevCtrl.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const api = require('../api')
 const co = require('co')
 const log = require('kth-node-log')
 const language = require('kth-node-web-common/lib/language')
@@ -8,8 +7,17 @@ const ReactDOMServer = require('react-dom/server')
 const { toJS } = require('mobx')
 const browserConfig = require('../configuration').browser
 const serverConfig = require('../configuration').server
-module.exports = {getCourseDevInfo: co.wrap(_getCourseDevInfo)}
+const {sortedKursutveckligApiInfo} = require('../apiCalls/kursutvecklingApi')
 
+
+module.exports = {
+  getCourseDevInfo: co.wrap(_getCourseDevInfo),
+  getCourseStaticDevInfo: co.wrap(_getCourseStaticDevInfo),
+}
+
+const serverPaths = require('../server').getPaths()
+
+//MOVE THIS PART TO OTHER CONTROLLER
 
 function hydrateStores (renderProps) {
   // This assumes that all stores are specified in a root element called Provider
@@ -35,23 +43,20 @@ function _staticRender(context, location) {
 }
 
 async function _getCourseDevInfo (req, res, next) {
-  
+
   const courseCode = req.params.courseCode
+  // const ldapUser = req.session.authUser ? req.session.authUser.username : 'null'
   const lang = language.getLanguage(res) || 'sv'
 
   try {
-    const paths = api.kursinfoApi.paths
-    // const respSellDesc = await _getSellingTextFromKursinfoApi(courseCode)
-    // const userKthId = req.session.authUser.ugKthid || ''
     // Render inferno app
-    const context = {}
-    const renderProps = _staticRender(context, req.url)
-    // renderProps.props.children.props.adminStore.setUser(userKthId)
-    await renderProps.props.children.props.adminStore.getCourseRequirementFromKopps(courseCode, lang)
-    // renderProps.props.children.props.adminStore.addSellingTextAndImage(respSellDesc.body)
-    // renderProps.props.children.props.adminStore.addChangedByLastTime(respSellDesc.body)
-    renderProps.props.children.props.adminStore.setBrowserConfig(browserConfig, paths, serverConfig.hostUrl)
+    // const context = {}
+    const renderProps = _staticRender()
+    renderProps.props.children.props.adminStore.setBrowserConfig(browserConfig, serverPaths, serverConfig.hostUrl)
     renderProps.props.children.props.adminStore.__SSR__setCookieHeader(req.headers.cookie)
+    await renderProps.props.children.props.adminStore.getCourseRequirementFromKopps(courseCode, lang)
+    renderProps.props.children.props.adminStore.analysisData = await sortedKursutveckligApiInfo(courseCode)
+    console.log("adminStore.analysisData in courseDevCtrl",renderProps.props.children.props.adminStore.analysisData )
     // await doAllAsyncBefore({
     //   pathname: req.originalUrl,
     //   query: (req.originalUrl === undefined || req.originalUrl.indexOf('?') === -1) ? undefined : req.originalUrl.substring(req.originalUrl.indexOf('?'), req.originalUrl.length),
@@ -59,9 +64,10 @@ async function _getCourseDevInfo (req, res, next) {
     //   routes: renderProps.props.children.props.children.props.children.props.children
     // })
     const html = ReactDOMServer.renderToString(renderProps)
-    console.log("HTNNNNNL", html)
     res.render('course/index', {
+      debug: 'debug' in req.query,
       html,
+      title: 'TODO',
       initialState: JSON.stringify(hydrateStores(renderProps))
     })
   } catch (err) {
@@ -69,4 +75,36 @@ async function _getCourseDevInfo (req, res, next) {
     next(err)
   }
 }
+//===================================//===================================//===================================//===================================
 
+async function _getCourseStaticDevInfo(req, res, next) {
+
+  const courseCode = req.params.courseCode
+  // const ldapUser = req.session.authUser ? req.session.authUser.username : 'null'
+  const lang = language.getLanguage(res) || 'sv'
+
+  try {
+    // Render inferno app
+    const context = {}
+    const renderProps = _staticRender()
+    renderProps.props.children.props.adminStore.setBrowserConfig(browserConfig, serverPaths, serverConfig.hostUrl)
+    renderProps.props.children.props.adminStore.__SSR__setCookieHeader(req.headers.cookie)
+    await renderProps.props.children.props.adminStore.getCourseRequirementFromKopps(courseCode, lang)
+    // await doAllAsyncBefore({
+    //   pathname: req.originalUrl,
+    //   query: (req.originalUrl === undefined || req.originalUrl.indexOf('?') === -1) ? undefined : req.originalUrl.substring(req.originalUrl.indexOf('?'), req.originalUrl.length),
+    //   adminStore: renderProps.props.children.props.adminStore,
+    //   routes: renderProps.props.children.props.children.props.children.props.children
+    // })
+    const html = ReactDOMServer.renderToString(renderProps)
+    res.render('course/index', {
+      debug: 'debug' in req.query,
+      html,
+      title: 'TODO',
+      initialState: JSON.stringify(hydrateStores(renderProps))
+    })
+  } catch (err) {
+    log.error('Error in _getCourseStaticDevInfo', { error: err })
+    next(err)
+  }
+}
