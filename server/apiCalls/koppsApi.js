c15c30fcb677d5f34aebca8d91a549cf0950879d
Elena Rakhimova <elenara@kth.se>
2019-06-17 18:12:10 +0200

    Changed kopps api endpoint and reformat fetched data, added filter for course plans

--- server/apiCalls/koppsApi.js
+++ server/apiCalls/koppsApi.js
@@ -4,7 +4,6 @@ const { server } = require('../configuration.js')
 const log = require('kth-node-log')
 const redis = require('kth-node-redis')
 
-
 const koppsApi = new BasicAPI({
   hostname: server.kopps.host,
   basePath: server.kopps.basePath,
@@ -20,7 +19,7 @@ const koppsApi = new BasicAPI({
 
 const koppsCourseData = async (courseCode) => {
   try {
-    const course = await koppsApi.getAsync({ uri: `course/${encodeURIComponent(courseCode)}`, useCache: true })
+    const course = await koppsApi.getAsync({ uri: `course/${encodeURIComponent(courseCode)}/detailedinformation`, useCache: true })
     return course.body
   } catch (error) {
     log.error('Exception calling from koppsAPI in koppsApi.koppsCourseData', { error })
@@ -28,17 +27,31 @@ const koppsCourseData = async (courseCode) => {
   }
 }
 
-function isValidData (dataObject, language = 0) {
-  return !dataObject ? EMPTY : dataObject
+function isValidData (dataObject) {
+  return !dataObject ? ' ' : dataObject
+}
+
+function getCoursePlanYears (publicSyllabusVersionsArr) {
+  let coursePlanYearsArr = []
+  if (publicSyllabusVersionsArr.length > 0) {  
+    for (let publicSyllabus of publicSyllabusVersionsArr) {
+      coursePlanYearsArr.push(publicSyllabus.validFromTerm.term)
+    }
+  }
+  let coursePlansPeriodFromToYearsArr = coursePlanYearsArr.sort().reverse()
+  if (coursePlansPeriodFromToYearsArr.length > 0) coursePlansPeriodFromToYearsArr.unshift('')
+  return coursePlansPeriodFromToYearsArr //[ '', 20182, 20082, 20082, 20081 ]
 }
 
 const filteredKoppsData = async (courseCode, lang) => {
   try {
-    const course = await koppsCourseData(courseCode)
+    const courseObj = await koppsCourseData(courseCode)
     const courseTitleData = {
-      course_code: isValidData(course.code),
-      course_title: isValidData(course.title[lang]),
-      course_credits: isValidData(course.credits),
+      course_code: isValidData(courseObj.course.courseCode),
+      course_title: isValidData(courseObj.course.title),
+      course_title_en: isValidData(courseObj.course.titleOther),
+      course_plans: getCoursePlanYears(courseObj.publicSyllabusVersions),
+      course_credits: isValidData(courseObj.course.credits),
       apiError: false
     }
     return {
@@ -58,4 +71,4 @@ const filteredKoppsData = async (courseCode, lang) => {
   }
 }
 
-module.exports = {filteredKoppsData}
+module.exports = { filteredKoppsData }
