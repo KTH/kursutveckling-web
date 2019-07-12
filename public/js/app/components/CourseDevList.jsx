e300d731149d53ac339ee7235b877bf93ce8de52
Elena Rakhimova <elenara@kth.se>
2019-07-12 17:54:27 +0500

    Refactored code for cousedevlist

--- public/js/app/components/CourseDevList.jsx
+++ public/js/app/components/CourseDevList.jsx
@@ -2,18 +2,24 @@ import TableForCourseRound from './TableForCourseRound'
 import React from 'react'
 import { KUTV_ADMIN_URL } from '../util/constants'
 
+
 const CourseDevAllRoundsForEachYear = ({arrOfRoundObj, koppsData, courseCode, year, translate}) => {
+  const linkToCreateNew = `${KUTV_ADMIN_URL}${courseCode}?l=${koppsData.lang}&status=n&serv=kutv`
   return (
     <span>
       <span className='navigation'>
         <h3>{year}</h3>
-        <a href={`${KUTV_ADMIN_URL}${courseCode}?l=${koppsData.lang}&status=n&serv=kutv`}>{translate.header_main_publish_new}</a>
+        <a href={linkToCreateNew}>{translate.header_main_publish_new}</a>
       </span>
       {arrOfRoundObj.length === 0
       ? <p>{translate.no_course_analys}</p>
       : arrOfRoundObj.map((roundObj, index) =>
         <span className='table-for-year' key={index}>
-          <p className='right-link'><a href={`${KUTV_ADMIN_URL}${roundObj._id}?l=${koppsData.lang}&serv=kutv&status=p&title=${koppsData.course_title}_${koppsData.course_credits}`}>{translate.header_main_edit}</a></p>
+          <p className='right-link'>
+            <a href={`${KUTV_ADMIN_URL}${roundObj._id}?l=${koppsData.lang}&serv=kutv&status=p&title=${koppsData.course_title}_${koppsData.course_credits}`}>
+              {translate.header_main_edit}
+            </a>
+          </p>
           <TableForCourseRound courseRoundObj={roundObj} courseAnalysDataId={roundObj._id} translate={translate} />
         </span>
       )
@@ -23,13 +29,16 @@ const CourseDevAllRoundsForEachYear = ({arrOfRoundObj, koppsData, courseCode, ye
 }
 
 const CourseDevAllYears = ({allYearsObj, koppsData, courseCode, translate}) => {
-  console.log('arrOfRoundObj', allYearsObj)
   const yearsDescending = Object.keys(allYearsObj).reverse()
   return (
     <div className='tables-list col'>
       {
         yearsDescending.map((year, index) =>
-          <CourseDevAllRoundsForEachYear key={index} arrOfRoundObj={allYearsObj[year]} koppsData={koppsData} courseCode={courseCode} year={year} translate={translate} />
+          <CourseDevAllRoundsForEachYear key={index}
+            arrOfRoundObj={allYearsObj[year]}
+            koppsData={koppsData} courseCode={courseCode}
+            year={year}
+            translate={translate} />
         )
       }
     </div>
