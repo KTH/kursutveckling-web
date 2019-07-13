c56e7ab982449158bd616882b8348a7eb7abca4c
Elena Rakhimova <elenara@kth.se>
2019-07-13 19:59:16 +0500

    Refactored: better naming and structure for course development list

---
+++ public/js/app/components/CourseDevAllYears.jsx
@@ -0,0 +1,50 @@
+import CourseDevelopmentForEachCourseRound from './CourseDevelopmentForEachCourseRound'
+import React from 'react'
+import { KUTV_ADMIN_URL } from '../util/constants'
+
+const CourseDevAllRoundsForEachYear = ({oneYearAnalysisDataArrOfObj, koppsData, year, translate}) => {
+  const koppsDataLang = koppsData.lang
+  const courseTitle = koppsData.courseTitle
+  const courseCredits = koppsData.courseCredits
+  const linkToCreateNew = `${KUTV_ADMIN_URL}${koppsData.courseCode}?l=${koppsDataLang}&status=n&serv=kutv`
+  return (
+    <span>
+      <span className='header-with-link'>
+        <h3>{year}</h3>
+        <a href={linkToCreateNew}>{translate.header_main_publish_new}</a>
+      </span>
+      {oneYearAnalysisDataArrOfObj.length === 0
+      ? <p>{translate.no_course_analys}</p>
+      : oneYearAnalysisDataArrOfObj.map((eachRoundObj, index) =>
+        <span className='table-for-year' key={index}>
+          <p className='right-link'>
+            <a href={`${KUTV_ADMIN_URL}${eachRoundObj._id}?l=${koppsDataLang}&serv=kutv&status=p&title=${courseTitle}_${courseCredits}`}>
+              {translate.header_main_edit}
+            </a>
+          </p>
+          <CourseDevelopmentForEachCourseRound courseRoundObj={eachRoundObj} courseAnalysDataId={eachRoundObj._id} translate={translate} />
+        </span>
+      )
+    }
+    </span>
+      )
+}
+
+const CourseDevAllYears = ({allYearsAnalysisDataObj, koppsData, translate}) => {
+  const yearsDescending = Object.keys(allYearsAnalysisDataObj).reverse()
+  return (
+    <div className='tables-list col'>
+      {
+        yearsDescending.map((year, index) =>
+          <CourseDevAllRoundsForEachYear key={index}
+            oneYearAnalysisDataArrOfObj={allYearsAnalysisDataObj[year]}
+            koppsData={koppsData}
+            year={year}
+            translate={translate} />
+        )
+      }
+    </div>
+  )
+}
+
+export default CourseDevAllYears
