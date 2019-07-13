f24b3198217237bbaf0520c0bbd5b894da1e5355
Elena Rakhimova <elenara@kth.se>
2019-07-13 17:21:30 +0500

    Refactored code to give a better naming if variables to improve code readability

--- public/js/app/components/CourseDevList.jsx
+++ public/js/app/components/CourseDevList.jsx
@@ -2,25 +2,27 @@ import TableForCourseRound from './TableForCourseRound'
 import React from 'react'
 import { KUTV_ADMIN_URL } from '../util/constants'
 
-
-const CourseDevAllRoundsForEachYear = ({arrOfRoundObj, koppsData, courseCode, year, translate}) => {
-  const linkToCreateNew = `${KUTV_ADMIN_URL}${courseCode}?l=${koppsData.lang}&status=n&serv=kutv`
+const CourseDevAllRoundsForEachYear = ({oneYearAnalysisDataArrOfObj, koppsData, year, translate}) => {
+  const koppsDataLang = koppsData.lang
+  const courseTitle = koppsData.courseTitle
+  const courseCredits = koppsData.courseCredits
+  const linkToCreateNew = `${KUTV_ADMIN_URL}${koppsData.courseCode}?l=${koppsDataLang}&status=n&serv=kutv`
   return (
     <span>
-      <span className='navigation'>
+      <span className='header-with-link'>
         <h3>{year}</h3>
         <a href={linkToCreateNew}>{translate.header_main_publish_new}</a>
       </span>
-      {arrOfRoundObj.length === 0
+      {oneYearAnalysisDataArrOfObj.length === 0
       ? <p>{translate.no_course_analys}</p>
-      : arrOfRoundObj.map((roundObj, index) =>
+      : oneYearAnalysisDataArrOfObj.map((eachRoundObj, index) =>
         <span className='table-for-year' key={index}>
           <p className='right-link'>
-            <a href={`${KUTV_ADMIN_URL}${roundObj._id}?l=${koppsData.lang}&serv=kutv&status=p&title=${koppsData.course_title}_${koppsData.course_credits}`}>
+            <a href={`${KUTV_ADMIN_URL}${eachRoundObj._id}?l=${koppsDataLang}&serv=kutv&status=p&title=${courseTitle}_${courseCredits}`}>
               {translate.header_main_edit}
             </a>
           </p>
-          <TableForCourseRound courseRoundObj={roundObj} courseAnalysDataId={roundObj._id} translate={translate} />
+          <TableForCourseRound courseRoundObj={eachRoundObj} courseAnalysDataId={eachRoundObj._id} translate={translate} />
         </span>
       )
     }
@@ -28,15 +30,15 @@ const CourseDevAllRoundsForEachYear = ({arrOfRoundObj, koppsData, courseCode, ye
       )
 }
 
-const CourseDevAllYears = ({allYearsObj, koppsData, courseCode, translate}) => {
-  const yearsDescending = Object.keys(allYearsObj).reverse()
+const CourseDevAllYears = ({allYearsAnalysisDataObj, koppsData, translate}) => {
+  const yearsDescending = Object.keys(allYearsAnalysisDataObj).reverse()
   return (
     <div className='tables-list col'>
       {
         yearsDescending.map((year, index) =>
           <CourseDevAllRoundsForEachYear key={index}
-            arrOfRoundObj={allYearsObj[year]}
-            koppsData={koppsData} courseCode={courseCode}
+            oneYearAnalysisDataArrOfObj={allYearsAnalysisDataObj[year]}
+            koppsData={koppsData}
             year={year}
             translate={translate} />
         )
