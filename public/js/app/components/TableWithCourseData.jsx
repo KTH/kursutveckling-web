c122bf97f457e8b7dba47c61fbc0842fcb6dca67
Elena Rakhimova <elenara@kth.se>
2019-07-15 17:38:26 +0500

    Rebuild table by using map, move function, and added popover for headers

--- public/js/app/components/TableWithCourseData.jsx
+++ public/js/app/components/TableWithCourseData.jsx
@@ -1,70 +1,61 @@
 import React from 'react'
-import { Table, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap'
+import { Table } from 'reactstrap'
+import { PopOverTextForTableHeaders, PopoverExamItem } from './PopOverTextForTable'
 
-const PopoverExamItem = ({header, shortAndLongExamStrArr, id}) => {
-  return (
-    <UncontrolledPopover trigger='click' placement='left-start' target={id}>
-      <PopoverHeader>{header}</PopoverHeader>
-      <PopoverBody>
-        {shortAndLongExamStrArr.map((shortAndLongTextStr, index) => <p className='popOver' key={index}>{shortAndLongTextStr[1]}</p>)}
-      </PopoverBody>
-    </UncontrolledPopover>
-  )
+const TableStandardCells = ({columnsArr, tableTitlesTranslation, popOverId, courseRoundData}) => {
+  return columnsArr.map((apiColName, index) =>
+    <td className={apiColName} id={apiColName + popOverId} data-label={tableTitlesTranslation[apiColName].header} key={index}>
+      <p>{courseRoundData[apiColName]}</p>
+    </td>
+    )
 }
 
-const TableWithCourseData = ({translate, courseRoundData, examPopOverId}) => {
-  function getShortAndLongStrForEachExam (examinationRoundsArr) {
-    let shortAndLongExamStrArr = []
-    if (examinationRoundsArr && examinationRoundsArr.length > 0) {
-      shortAndLongExamStrArr = examinationRoundsArr.map(row => {
-        let examInfoArr = row.trim().split(';')
-        let shortStr = `${examInfoArr[0]} (${examInfoArr[2]}) ${examInfoArr[5]}` || ''
-        let longStr = `${examInfoArr[0]} - ${examInfoArr[1]},  ${examInfoArr[2]} ${examInfoArr[3]}, ${examInfoArr[4]}: ${examInfoArr[5]}` || ''
-        return [shortStr, longStr]
-      })
-    }
-    return shortAndLongExamStrArr
+function _getShortAndLongStrForEachExam (examinationRoundsArr) {
+  let shortAndLongExamStrArr = []
+  if (examinationRoundsArr && examinationRoundsArr.length > 0) {
+    shortAndLongExamStrArr = examinationRoundsArr.map(row => {
+      let examInfoArr = row.trim().split(';')
+      let shortStr = `${examInfoArr[0]} (${examInfoArr[2]}) ${examInfoArr[5]}` || ''
+      let longStr = `${examInfoArr[0]} - ${examInfoArr[1]},  ${examInfoArr[2]} ${examInfoArr[3]}, ${examInfoArr[4]}: ${examInfoArr[5]}` || ''
+      return [shortStr, longStr]
+    })
   }
-  const shortAndLongExamStrArr = getShortAndLongStrForEachExam(courseRoundData.examinationRounds)
+  return shortAndLongExamStrArr
+}
 
+const TableWithCourseData = ({translate, courseRoundObj}) => {
+  const orderedColumns = ['responsibles', 'examiners', 'registeredStudents', 'examShortAndLongStrArr', 'examinationGrade', 'alterationText']
+  const examShortAndLongStrArr = _getShortAndLongStrForEachExam(courseRoundObj.examinationRounds)
+  const popOverId = courseRoundObj._id
   return (
-    <span>
+    <span className='table-for-each-round' key={popOverId}>
       <Table>
         <thead>
           <tr>
-            <th>{translate.header_responsibles}</th>
-            <th>{translate.header_examiners}</th>
-            <th alt='Antal reg studenter'>{translate.header_registrated}</th>
-            <th>{translate.header_examination}</th>
-            <th alt='; i % av aktiva (totalt) vid första ex-tillfället Examinationsgrad'>{translate.header_examination_grade}</th>
-            <th>{translate.header_course_changes_comment}</th>
+            {orderedColumns.map((apiColName, index) =>
+              <th id={popOverId + index} key={index}>
+                {translate[apiColName].header}
+              </th>
+            )}
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className='teacher-cell' data-label={translate.header_responsibles}>
-              <p>{courseRoundData.responsibles}</p>
-            </td>
-            <td className='teacher-cell' data-label={translate.header_examiners}>
-              <p>{courseRoundData.examiners}</p>
-            </td>
-            <td className='students-cell' data-label={translate.header_registrated}>
-              {courseRoundData.registeredStudents}
-            </td>
-            <td className='exam-cell' id={examPopOverId} data-label={translate.header_examination}>
-              {shortAndLongExamStrArr.map((shortAndLongTextStr, index) => <p key={index}>{shortAndLongTextStr[0]}</p>)}
+            <TableStandardCells columnsArr={orderedColumns.slice(0,3)}
+              tableTitlesTranslation={translate}
+              popOverId={popOverId} courseRoundData={courseRoundObj} />
+            <td className='examShortAndLongStrArr' id={'examShortAndLongStrArr'+popOverId} data-label={translate.examShortAndLongStrArr.header}>
+              {examShortAndLongStrArr.map((shortAndLongTextStr, index) => <p key={index}>{shortAndLongTextStr[0]}</p>)}
               <p><i>{translate.popover_more}</i></p>
             </td>
-            <td className='result-cell' data-label={translate.header_examination_grade}>
-              <p>{courseRoundData.examinationGrade} %</p>
-            </td>
-            <td data-label={translate.header_course_changes_comment}>
-              <p>{courseRoundData.alterationText}</p>
-            </td>
+            <TableStandardCells columnsArr={orderedColumns.slice(4,)}
+              tableTitlesTranslation={translate}
+              popOverId={popOverId} courseRoundData={courseRoundObj} />  
           </tr>
         </tbody>
       </Table>
-      <PopoverExamItem header={translate.header_examination} shortAndLongExamStrArr={shortAndLongExamStrArr} id={examPopOverId} />
+      <PopOverTextForTableHeaders columnsArr={orderedColumns} translate={translate} popOverId={popOverId}/>
+      <PopoverExamItem examShortAndLongStrArr={examShortAndLongStrArr} id={'examShortAndLongStrArr'+popOverId} />
     </span>
   )
 }
