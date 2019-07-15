48fe6715218163a805e65f357669019477c1d160
Elena Rakhimova <elenara@kth.se>
2019-07-15 17:36:55 +0500

    Refactored code for table links, removed some code

--- public/js/app/components/SyllabusPmAnalysLinks.jsx
+++ public/js/app/components/SyllabusPmAnalysLinks.jsx
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { SYLLABUS_URL } from '../util/constants'
 
 const ActiveOrDisavledLink = ({href, linkTitle, validFrom}) => {
-  let isDisabled
-  href === '#' ? isDisabled = true : isDisabled = false
   return (
     <p>
-        {isDisabled
+        {href === '#'
         ? <a className='pdf-link' key={linkTitle}>
             {linkTitle}: -
         </a>
@@ -17,18 +15,17 @@ const ActiveOrDisavledLink = ({href, linkTitle, validFrom}) => {
     )
 }
 
-const SyllabusPmAnalysLinks = ({translate, courseRoundData, storageUri, koppsData}) => {
-  const analysisLink = courseRoundData.analysisFileName !== '' ? storageUri + courseRoundData.analysisFileName : '#'
-  const analysisPublishedDate = courseRoundData.pdfAnalysisDate
-  const syllabusRawStartTerm = courseRoundData.syllabusStartTerm
-  const syllabusHref = syllabusRawStartTerm ? `${SYLLABUS_URL}${koppsData.course_code}-${syllabusRawStartTerm}` : '#'
-  const syllabusPublishedDate = syllabusRawStartTerm ? `${translate.course_short_semester[syllabusRawStartTerm.toString().substring(4, 5)]} ${syllabusRawStartTerm.toString().substring(0, 4)}` : ''
+const SyllabusPmAnalysLinks = ({translate, courseRoundObj, storageUri, koppsData}) => {
+  const { pdfAnalysisDate, syllabusStartTerm, analysisFileName } = courseRoundObj
+  const analysisLink = analysisFileName !== '' ? storageUri + analysisFileName : '#'
+  const syllabusHref = syllabusStartTerm ? `${SYLLABUS_URL}${koppsData.course_code}-${syllabusStartTerm}` : '#'
+  const syllabusPublishedDate = syllabusStartTerm ? `${translate.course_short_semester[syllabusStartTerm.toString().substring(4, 5)]} ${syllabusStartTerm.toString().substring(0, 4)}` : ''
 
   return (
     <span className='right-links' >
       <ActiveOrDisavledLink href={syllabusHref} linkTitle={translate.link_syllabus} validFrom={syllabusPublishedDate} />
       <ActiveOrDisavledLink href='https://kth.box.com/s/i9xu34n5conqdoj7re81bmcto20wavib' linkTitle={translate.link_pm} validFrom='2019-05-20' />
-      <ActiveOrDisavledLink href={analysisLink} linkTitle={translate.link_analysis} validFrom={analysisPublishedDate} />
+      <ActiveOrDisavledLink href={analysisLink} linkTitle={translate.link_analysis} validFrom={pdfAnalysisDate} />
     </span>
     )
 }
