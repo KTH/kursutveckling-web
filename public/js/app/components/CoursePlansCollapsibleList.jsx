4fbcbc81998572fb85124840abbf0c601eda211b
Elena Rakhimova <elenara@kth.se>
2019-08-07 14:34:24 +0200

    Fixed error for end date for course syllabus

--- public/js/app/components/CoursePlansCollapsibleList.jsx
+++ public/js/app/components/CoursePlansCollapsibleList.jsx
@@ -3,7 +3,6 @@ import { Collapse } from 'reactstrap'
 import { SYLLABUS_URL} from '../util/constants'
 import { inject, observer} from 'mobx-react'
 
-
 @inject(['adminStore']) @observer
 class CoursePlansCollapsibleList extends Component {
   constructor (props) {
@@ -30,21 +29,23 @@ class CoursePlansCollapsibleList extends Component {
               {syllabusSemesterList.length > 0
                 ? syllabusSemesterList.map((tillSemester, index, semesterArr) => {
                     if (semesterArr[index+1]) {
-                      const fromSemester = semesterArr[index+1]
+                      const startSemester = semesterArr[index+1]
+                      let lastTerm = tillSemester.toString().substring(4, 5)
+                      if ( lastTerm === '2' ) tillSemester -= 1
+                      else if (lastTerm === '1' ) tillSemester -= 9 
                       return (
                         <p key={index}>
                           <a alt={translate.alt_label_syllabus_link}
-                          href={`${SYLLABUS_URL}${courseCode}-${fromSemester}.pdf?lang=${lang}`}
-                          key={index}
-                          id={fromSemester}
-                          target='_blank'
-                          className='pdf-link'
-                          >
-                          {translate.label_syllabus_link} {' '}
-                          ( {translate.course_short_semester[fromSemester.toString().substring(4, 5)]}
-                          {fromSemester.toString().substring(0, 4)} -  &nbsp;
-                          {translate.course_short_semester[tillSemester.toString().substring(4, 5)]}
-                          {tillSemester.toString().substring(0, 4)} )
+                          href={`${SYLLABUS_URL}${courseCode}-${startSemester}.pdf?lang=${lang}`}
+                          key={index} id={startSemester} target='_blank'
+                          className='pdf-link'> 
+                            {translate.label_syllabus_link} {' ( '}
+                            {/* START SEMESTER FOR THIS COURSE SYLLABUS, f.e., Autumn 2009*/}
+                            {translate.course_short_semester[startSemester.toString().substring(4, 5)]}
+                            {startSemester.toString().substring(0, 4)} -  &nbsp;
+                            {/* LAST SEMESTER FOR THIS COURSE SYLLABUS f.e., Spring 2019*/}
+                            {translate.course_short_semester[tillSemester.toString().substring(4, 5)]}
+                            {tillSemester.toString().substring(0, 4)} {' )'}
                           </a>
                         </p>
                       )
