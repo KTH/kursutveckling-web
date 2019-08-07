0c36c295a0bdcadac2628611806cc88ad8021ce1
Elena Rakhimova <elenara@kth.se>
2019-08-07 15:57:37 +0200

    Fixes for alert message and added some target keys to popover elements

--- public/js/app/components/AlertMsg.jsx
+++ public/js/app/components/AlertMsg.jsx
@@ -7,14 +7,21 @@ const AlertMsg = ({props, translate, lang}) => {
         .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {})
 
   return (
-    params.serv === 'kutv' && params.term && params.name
+    params.serv === 'kutv' // && params.term && params.name
         ? params.event === 'save' || params.event === 'pub' || params.event === 'delete'
             ? <Alert color='success' aria-live='polite'>
               <h4>{translate.alertMessages['kutv'][params.event]}</h4>
-              <p>{translate.alertMessages.term}: {translate.course_short_semester[params.term.toString().substring(4, 5)]}
-                {params.term.toString().substring(0, 4)}
-              </p>
-              <p>{translate.alertMessages.course_round}: {decodeURIComponent(params.name)}</p>
+              {params.term
+                ? <p>{translate.alertMessages.term} {': '}
+                    {translate.course_short_semester[params.term.toString().substring(4, 5)]}
+                    {params.term.toString().substring(0, 4)}
+                  </p>
+                : ''
+              }
+              {params.name
+                ? <p>{translate.alertMessages.course_round}: {decodeURIComponent(params.name)}</p>
+                : ''
+              }
               {params.event === 'save'
                 ? <p>{translate.alertMessages.kutv.s_msg}</p>
                 : ''
