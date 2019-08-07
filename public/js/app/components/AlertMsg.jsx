80ae712a7bac6082b8dac0706e6c0f532107d662
Elena Rakhimova <elenara@kth.se>
2019-08-07 15:39:45 +0200

    removed some code

--- public/js/app/components/AlertMsg.jsx
+++ public/js/app/components/AlertMsg.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Alert } from 'reactstrap'
-import { KTH_SE_URL, COURSE_UTVECKLING } from '../util/constants'
 
 const AlertMsg = ({props, courseCode, translate, lang}) => {
   const params = props.location.search.substring(1).split('&')
         .map(param => param.split('='))
         .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {})
-  console.log('this.propsdsdasda', props.location)
 
   return (
     params.serv === 'kutv' && params.term && params.name
