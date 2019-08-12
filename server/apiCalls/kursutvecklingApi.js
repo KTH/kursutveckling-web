d83f6ff4db397f1a36885ae7cab2725ea074d7bc
Elena Rakhimova <elenara@kth.se>
2019-08-12 15:43:27 +0200

    Refactored and added asteriks if data manually edited and percentage for results

--- server/apiCalls/kursutvecklingApi.js
+++ server/apiCalls/kursutvecklingApi.js
@@ -19,23 +19,30 @@ async function _getKursutvecklingApiInfo (courseCode) {
 async function sortedKursutveckligApiInfo (courseCode) { //TODO: CACHE
   try {
     const kursutvApiResponse = await _getKursutvecklingApiInfo(courseCode)
-    const arrOfNonFilteredObj = kursutvApiResponse.body
+    const arrOfNonSortedObj = kursutvApiResponse.body
     const thisYear = new Date().getFullYear()
     let objYear
     let sortedByYear = {}
     let i=0
-    while (i <= 5) {//initialize for 10 years f.e. 2009-2019
-            sortedByYear[thisYear-i] = []
-            i++
+    while (i <= 5) {
+      //initialize array for each year in range of last 5 years f.e. 2014-2019 to save all course analysis obj
+      // { 2019: [], 2018: [], ..., 2014: []}
+      sortedByYear[thisYear-i] = []
+      i++
+    }
+    for (obj of arrOfNonSortedObj) {
+      objYear = obj.semester.substr(0,4)
+      //if = true manually edited
+      if (obj.isPublished === true) {
+        obj.examinationGrade = obj.examinationGradeFromLadok === false ? obj.examinationGrade + ' %' : obj.examinationGrade + ' % *'
+        obj.registeredStudents = obj.registeredStudentsFromLadok === false ? obj.registeredStudents : obj.registeredStudents + ' *'
+        if (sortedByYear[objYear])
+          sortedByYear[objYear].push(obj)
+        else { //typ sortedOlderThan5YearsAgo
+          //Let's check it there is course development for more than 5 years ago, f.e. 2007
+          sortedByYear[objYear] = [obj] 
         }
-    for (obj of arrOfNonFilteredObj) {
-        objYear = obj.semester.substr(0,4)
-        if (sortedByYear[objYear] && obj.isPublished === true)
-            sortedByYear[objYear].push(obj)
-        else if (obj.isPublished === true) { //TODO: Maybe it is better to move it to a separate function typ sortedOlderThan10YearsAgo
-            //Let's check it there is course development for more than 10 years ago, f.e. 2007
-            sortedByYear[objYear] = [obj] 
-        }
+      }
     }
     return sortedByYear
   } catch (error) {
