75a7da1d477e34fbcec86f8a8fe0313127396a63
Rickard Falk <rickardfalk@n154-p67.eduroam.kth.se>
2019-12-03 08:57:42 +0100

    Use datetime for published related dates

--- public/js/app/components/CollapseExtraInfo.jsx
+++ public/js/app/components/CollapseExtraInfo.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { Collapse } from 'reactstrap'
-import { getDateFormat } from '../util/helpers'
+import { formatISODate } from '../util/helpers'
 
-const ExtraKoppsInfo = ({translate, courseRoundObj}) => {
+const ExtraKoppsInfo = ({ translate, courseRoundObj }) => {
   const popOverId = courseRoundObj._id
   const orderedColumns = ['commentExam', 'programmeCodes', 'analysisName']
   return (
@@ -13,33 +13,32 @@ const ExtraKoppsInfo = ({translate, courseRoundObj}) => {
             <p id={popOverId + index} key={'header-for-' + apiName}><b>{translate[apiName].header}</b></p>
             {courseRoundObj[apiName] === ''
               ? <p className='textBlock'> <i>{translate.no_added}</i></p>
-              : <p className='textBlock' dangerouslySetInnerHTML={{__html: courseRoundObj[apiName]}}></p>
-            }
+              : <p className='textBlock' dangerouslySetInnerHTML={{ __html: courseRoundObj[apiName] }} />}
           </span>
         )
       }
     </span>
   )
 }
-const ExtraDatesAndComment = ({translate, courseRoundObj}) => {
+const ExtraDatesAndComment = ({ translate, courseRoundObj }) => {
   return (
     <span>
       <p><b>{translate.header_publishing_dates}</b></p>
       <p>{translate.publishedDate}:&nbsp;
-          {getDateFormat(courseRoundObj.publishedDate, translate.page_lang)}</p>
+        {formatISODate(courseRoundObj.publishedDate, translate.page_lang)}
+      </p>
       {courseRoundObj.changedAfterPublishedDate && courseRoundObj.changedAfterPublishedDate !== ''
         ? <span>
           <p>{translate.changedAfterPublishedDate}:&nbsp;
-              {getDateFormat(courseRoundObj.changedAfterPublishedDate, translate.page_lang)}</p>
+            {formatISODate(courseRoundObj.changedAfterPublishedDate, translate.page_lang)}
+          </p>
           <p>{translate.commentChange}:</p>
           <p>{courseRoundObj.commentChange === ''
-              ? <i>{translate.no_added}</i>
-              : courseRoundObj.commentChange
-              }
+            ? <i>{translate.no_added}</i>
+            : courseRoundObj.commentChange}
           </p>
-        </span>
-        : <p>{translate.changedAfterPublishedDate}:&nbsp;<i>{translate.no_date_last_changed}</i></p>
-      }
+          </span>
+        : <p>{translate.changedAfterPublishedDate}:&nbsp;<i>{translate.no_date_last_changed}</i></p>}
     </span>
   )
 }
@@ -47,21 +46,26 @@ class CollapseExtraInfo extends Component {
   constructor (props) {
     super(props)
     this.toggleHeader = this.toggleHeader.bind(this)
-    this.state = {collapseExtraInfo: this.props.isOpen}
+    this.state = { collapseExtraInfo: this.props.isOpen }
   }
+
   toggleHeader (event) {
     event.preventDefault()
-    this.setState(state => ({collapseExtraInfo: !state.collapseExtraInfo}))
+    this.setState(state => ({ collapseExtraInfo: !state.collapseExtraInfo }))
   }
+
   render () {
     const { courseRoundObj, label, translate } = this.props
     return (
-      <div className='card collapsible rubric-list white' >
+      <div className='card collapsible rubric-list white'>
         <div className='card-header info-rubric' role='tab' onClick={this.toggleHeader}>
           <h4 className='mb-0'>
-            <a href='#more' className='collapse-header title' id={label}
+            <a
+href='#more' className='collapse-header title' id={label}
               aria-expanded={this.state.collapseExtraInfo} load='false' data-toggle='collapse'
-              alt={`${translate.alt_header_more_info} ${courseRoundObj.analysisName}`}>{translate.header_more_info}</a>
+              alt={`${translate.alt_header_more_info} ${courseRoundObj.analysisName}`}
+            >{translate.header_more_info}
+            </a>
           </h4>
         </div>
         <Collapse color='white' isOpen={this.state.collapseExtraInfo} toggler={label}>
@@ -71,8 +75,8 @@ class CollapseExtraInfo extends Component {
           </div>
         </Collapse>
       </div>
-      )
+    )
   }
-  }
+}
 
 export default CollapseExtraInfo
--- public/js/app/util/helpers.js
+++ public/js/app/util/helpers.js
@@ -7,3 +7,22 @@ export const getDateFormat = (date, language) => {
   const splitDate = date.split('-')
   return `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`
 }
+
+export const formatISODate = (date, lang) => {
+  if (date === '') return null
+  const timestamp = Date.parse(date)
+  const parsedDate = new Date(timestamp)
+  let languageTag // BCP 47 language tag
+  switch (lang) {
+    case 'Svenska':
+    case 'Engelska':
+    case 1:
+    case 'sv':
+      languageTag = 'sv-SE'
+      break
+    default:
+      languageTag = 'en-US'
+      break
+  }
+  return parsedDate.toLocaleDateString(languageTag)
+}
