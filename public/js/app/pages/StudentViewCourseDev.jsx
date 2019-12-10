564b21e53ea031d6246a868deb342f30849cf1ef
Elena Rakhimova <elenara@kth.se>
2019-12-10 13:57:13 +0100

    minor refactor of pageTitle import

--- public/js/app/pages/StudentViewCourseDev.jsx
+++ public/js/app/pages/StudentViewCourseDev.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { inject, observer} from 'mobx-react'
 import i18n from '../../../../i18n'
 
-import CourseTitle from '../components/CourseTitle'
+import PageTitle from '../components/PageTitle'
 import KipLinkNav from '../components/KipNav'
 import CourseDevAllYears from '../components/CourseDevAllYears'
 import AlertMsg from '../components/AlertMsg'
@@ -31,7 +31,7 @@ class StudentViewCourseDev extends Component {
     return (
       <div key='kursinfo-container' className='kursinfo-main-page col' >
         {/* ---COURSE TITEL--- */}
-        <CourseTitle key='title'
+        <PageTitle key='title'
           courseKoppsData={courseKoppsData}
           pageTitle={pageTitles.course_dev_title}
           />
