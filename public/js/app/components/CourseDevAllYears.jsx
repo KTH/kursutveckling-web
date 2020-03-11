fb462dd102496433ddd63f810c9fe20d1a5c287d
Rickard Falk <rickard.falk@gmail.com>
2020-03-11 15:28:33 +0100

    Sort course analyses by semester

--- public/js/app/components/CourseDevAllYears.jsx
+++ public/js/app/components/CourseDevAllYears.jsx
@@ -2,9 +2,14 @@ import CourseDevelopmentForEachCourseRound from './CourseDevelopmentForEachCours
 import React from 'react'
 import { KUTV_ADMIN_URL } from '../util/constants'
 
-const CourseDevAllRoundsForEachYear = ({oneYearAnalysisDataArrOfObj, koppsData, year, translate}) => {
+const CourseDevAllRoundsForEachYear = ({ oneYearAnalysisDataArrOfObj, koppsData, year, translate }) => {
   const { courseCode, courseTitle, courseCredits, koppsDataLang } = koppsData
   const linkToCreateNew = `${KUTV_ADMIN_URL}${courseCode}?l=${koppsDataLang}&status=n&serv=kutv`
+
+  // Sort analyses, so fall semester courses come before spring semester courses
+  oneYearAnalysisDataArrOfObj.sort((firstEl, secondEl) =>
+    (secondEl.semester > firstEl.semester) ? 1 : ((firstEl.semester > secondEl.semester) ? -1 : 0))
+
   return (
     <span>
       <span className='header-with-link'>
@@ -12,35 +17,38 @@ const CourseDevAllRoundsForEachYear = ({oneYearAnalysisDataArrOfObj, koppsData, 
         <a href={linkToCreateNew} className='right-link' alt={`${translate.alt_header_main_publish_new} ${year}`}>{translate.header_main_publish_new}</a>
       </span>
       {oneYearAnalysisDataArrOfObj.length === 0
-      ? <p>{translate.no_course_analys}</p>
-      : oneYearAnalysisDataArrOfObj.map((eachRoundObj, index) =>
-        <span className='table-for-year' key={index}>
-          <p className='right-link'>
-            <a href={`${KUTV_ADMIN_URL}${eachRoundObj._id}?l=${koppsDataLang}&serv=kutv&status=p&title=${courseTitle}_${courseCredits}`}
-              alt={`${translate.alt_header_main_edit} ${eachRoundObj.analysisName}`}>
-              {translate.header_main_edit}
-            </a>
-          </p>
-          <CourseDevelopmentForEachCourseRound courseRoundObj={eachRoundObj} translate={translate} />
-        </span>
-      )
-    }
+        ? <p>{translate.no_course_analys}</p>
+        : oneYearAnalysisDataArrOfObj.map((eachRoundObj, index) =>
+          <span className='table-for-year' key={index}>
+            <p className='right-link'>
+              <a
+                href={`${KUTV_ADMIN_URL}${eachRoundObj._id}?l=${koppsDataLang}&serv=kutv&status=p&title=${courseTitle}_${courseCredits}`}
+                alt={`${translate.alt_header_main_edit} ${eachRoundObj.analysisName}`}
+              >
+                {translate.header_main_edit}
+              </a>
+            </p>
+            <CourseDevelopmentForEachCourseRound courseRoundObj={eachRoundObj} translate={translate} />
+          </span>
+        )}
     </span>
-      )
+  )
 }
 
-const CourseDevAllYears = ({allYearsAnalysisDataObj, koppsData, translate}) => {
+const CourseDevAllYears = ({ allYearsAnalysisDataObj, koppsData, translate }) => {
   const yearsDescending = Object.keys(allYearsAnalysisDataObj).reverse()
   return (
     <div className='tables-list col'>
       <p>{translate.info_manually_edited}</p>
       {
         yearsDescending.map((year, index) =>
-          <CourseDevAllRoundsForEachYear key={index}
+          <CourseDevAllRoundsForEachYear
+            key={index}
             oneYearAnalysisDataArrOfObj={allYearsAnalysisDataObj[year]}
             koppsData={koppsData}
             year={year}
-            translate={translate} />
+            translate={translate}
+          />
         )
       }
     </div>
