9b0d3f65a9d083308cafc34ab0314e2909c4bde3
Elena Rakhimova <elenara@kth.se>
2020-08-26 19:32:35 +0200

    Added key for popover texts

--- public/js/app/components/PopOverTextForTable.jsx
+++ public/js/app/components/PopOverTextForTable.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import { UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap'
 
-export const PopOverTextForTableHeaders = ({ translate, columnsArr, popOverId }) => {
-  return columnsArr.map((apiColName, index) => (
-    <span>
-      <UncontrolledPopover
-        trigger="legacy"
-        placement="auto"
-        target={popOverId + apiColName}
-        key={'desktop' + index}
-        className="header-popup"
-      >
-        <PopoverHeader>{translate[apiColName].header}</PopoverHeader>
-        <PopoverBody>{translate[apiColName].popoverText}</PopoverBody>
-      </UncontrolledPopover>
-      <UncontrolledPopover
-        trigger="legacy"
-        placement="auto"
-        target={'labelfor' + popOverId + apiColName}
-        key={'mobile' + index}
-        className="header-popup"
-      >
-        <PopoverHeader>{translate[apiColName].header}</PopoverHeader>
-        <PopoverBody>{translate[apiColName].popoverText}</PopoverBody>
-      </UncontrolledPopover>
-    </span>
-  ))
-}
+export const PopOverTextForTableHeaders = ({ translate, columnsArr, popOverId }) =>
+  columnsArr.map((colName, index) => {
+    const { header, popoverText } = translate[colName]
+
+    return (
+      <span key={'hiddenTexts-For-' + popOverId + colName}>
+        <UncontrolledPopover
+          trigger="legacy"
+          placement="auto"
+          target={'desktopPopOver' + popOverId + colName}
+          key={'keyforDesktop' + popOverId + colName}
+          className="header-popup"
+        >
+          <PopoverHeader>{header}</PopoverHeader>
+          <PopoverBody>{popoverText}</PopoverBody>
+        </UncontrolledPopover>
+        <UncontrolledPopover
+          trigger="legacy"
+          placement="auto"
+          target={'labelforMobilePopOver' + popOverId + colName}
+          key={'keyforMobilePopOver' + popOverId + colName}
+          className="header-popup"
+        >
+          <PopoverHeader>{header}</PopoverHeader>
+          <PopoverBody>{popoverText}</PopoverBody>
+        </UncontrolledPopover>
+      </span>
+    )
+  })
