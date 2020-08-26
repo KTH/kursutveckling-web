b5d137ef1fc09328f5a7bbae161ecfe2d2a26c91
Elena Rakhimova <elenara@kth.se>
2020-08-26 19:32:51 +0200

    Lint and fixed typo err

--- public/js/app/components/SyllabusPmAnalysLinks.jsx
+++ public/js/app/components/SyllabusPmAnalysLinks.jsx
@@ -2,47 +2,71 @@ import React from 'react'
 import { SYLLABUS_URL } from '../util/constants'
 import { getDateFormat } from '../util/helpers'
 
-const ActiveOrDisavledLink = ({ fileName, storageUri, linkTitle, alt, validFrom }) => {
+const ActiveOrDisabledLink = ({ fileName, storageUri, linkTitle, ariaLabel, validFrom }) => {
   return (
     <p>
-        {fileName === ''
-        ? <a className='pdf-link' key={linkTitle}>
-            {linkTitle}: -
+      {fileName === '' ? (
+        <a className="pdf-link" key={linkTitle}>
+          {linkTitle}: -
         </a>
-        : <a href={`${storageUri}${fileName}`} className='pdf-link' key={linkTitle} alt={alt} target='_blank'>
-            {linkTitle}: {validFrom}
-        </a>}
+      ) : (
+        <a
+          href={`${storageUri}${fileName}`}
+          className="pdf-link"
+          key={linkTitle}
+          aria-label={ariaLabel}
+          target="_blank"
+        >
+          {linkTitle}: {validFrom}
+        </a>
+      )}
     </p>
-    )
+  )
 }
 
-const SyllabusPmAnalysLinks = ({translate, courseRoundObj, storageUri, lang}) => {
-  const { courseCode, syllabusStartTerm, analysisFileName, pdfAnalysisDate,
-    pmFileName, pdfPMDate } = courseRoundObj
+const SyllabusPmAnalysLinks = ({ translate, courseRoundObj, storageUri, lang }) => {
+  const {
+    courseCode,
+    syllabusStartTerm,
+    analysisFileName,
+    pdfAnalysisDate,
+    pmFileName,
+    pdfPMDate
+  } = courseRoundObj
   const syllabusFileName = syllabusStartTerm
-        ? `${courseCode}-${syllabusStartTerm}.pdf?lang=${lang}` : ''
+    ? `${courseCode}-${syllabusStartTerm}.pdf?lang=${lang}`
+    : ''
   const syllabusPublishedDate = syllabusStartTerm
-        ? `${translate.course_short_semester[syllabusStartTerm.toString().substring(4, 5)]} ${syllabusStartTerm.toString().substring(0, 4)}` : ''
+    ? `${
+        translate.course_short_semester[syllabusStartTerm.toString().substring(4, 5)]
+      } ${syllabusStartTerm.toString().substring(0, 4)}`
+    : ''
 
   return (
-    <span className='right-links' >
-      <ActiveOrDisavledLink fileName={syllabusFileName}
+    <span className="right-links">
+      <ActiveOrDisabledLink
+        fileName={syllabusFileName}
         storageUri={SYLLABUS_URL}
-        linkTitle={translate.link_syllabus} alt={translate.alt_link_syllabus}
+        linkTitle={translate.link_syllabus}
+        ariaLabel={translate.alt_link_syllabus}
         validFrom={syllabusPublishedDate}
-        />
-      <ActiveOrDisavledLink fileName={pmFileName}
+      />
+      <ActiveOrDisabledLink
+        fileName={pmFileName}
         storageUri={storageUri}
-        linkTitle={translate.link_pm} alt={translate.alt_link_pm}
+        linkTitle={translate.link_pm}
+        ariaLabel={translate.alt_link_pm}
         validFrom={getDateFormat(pdfPMDate, lang)}
-        />
-      <ActiveOrDisavledLink fileName={analysisFileName}
+      />
+      <ActiveOrDisabledLink
+        fileName={analysisFileName}
         storageUri={storageUri}
-        linkTitle={translate.link_analysis} alt={translate.alt_link_analysis}
+        linkTitle={translate.link_analysis}
+        ariaLabel={translate.alt_link_analysis}
         validFrom={getDateFormat(pdfAnalysisDate, lang)}
-        />
+      />
     </span>
-    )
+  )
 }
 
 export default SyllabusPmAnalysLinks
