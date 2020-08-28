1748e2778cf6cf0f90d96b54ba68b286deae1299
Elena Rakhimova <elenara@kth.se>
2020-08-28 12:05:47 +0200

    Refactor popover, fixed unique ids, added aria-pressed

--- public/js/app/components/PopOverTextForTable.jsx
+++ public/js/app/components/PopOverTextForTable.jsx
@@ -2,23 +2,26 @@ import React, { Component } from 'react'
 import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap'
 import i18n from '../../../../i18n'
 
-const OnlyMobileVisiblePopup = ({ ariaLabel, popUpHeader, id, onClick }) => {
+const OnlyMobileVisiblePopup = ({ ariaLabel, ariaPressed, popUpHeader, id, onClick }) => {
+  const mobilePopoverId = 'mobile-table-header' + popUpHeader + id
   return (
     <span
       className="mobile-header-popovers"
       key={'onlyForMobileView' + popUpHeader + id}
       role="info icon button"
-      aria-labelledby="mobile-table-header"
+      aria-labelledby={mobilePopoverId}
     >
-      <label id="mobile-table-header">{popUpHeader}</label>{' '}
+      <label id={mobilePopoverId} className="d-none d-sm-block d-md-nonee">
+        {popUpHeader}
+      </label>{' '}
       <Button
         id={id}
         type="button"
         className="mobile btn-info-modal"
         onClick={onClick}
         aria-label={ariaLabel}
-        role="info icon"
-      />{' '}
+        aria-pressed={ariaPressed}
+      />
     </span>
   )
 }
@@ -34,45 +37,49 @@ class ControlledPopover extends Component {
   }
 
   render() {
-    const { describesId, header, popoverText, popType, targetId } = this.props
+    const { cellId, describesId, header, popoverText, popType } = this.props
     const { popoverOpen } = this.state
+    const triggerId = `${popType}-popover-${cellId}`
+    const dialogHeaderId = `${popType}-dialog-header-${cellId}`
+    const dialogBodyId = `${popType}-dialog-header-${cellId}`
     const tableLang = i18n.isSwedish() ? 1 : 0
     const { aria_label_info_icon: ariaLabel, aria_label_close_icon: closeAria } = i18n.messages[
       tableLang
     ].tableHeaders
 
     return (
-      <span role="popover" role="info icon button" aria-labelledby={targetId}>
+      <span role="dialog" aria-labelledby={dialogHeaderId} aria-describedby={dialogBodyId}>
         {(popType === 'mobile' && (
           <OnlyMobileVisiblePopup
             popUpHeader={header}
-            id={targetId}
+            id={triggerId}
             onClick={this.toggle}
             ariaLabel={ariaLabel}
+            ariaPressed={popoverOpen}
           />
         )) || (
           <Button
-            id={targetId}
+            id={triggerId}
             type="button"
             className="desktop btn-info-modal"
             onClick={this.toggle}
             aria-label={ariaLabel}
+            aria-pressed={popoverOpen}
           />
         )}
         <Popover
           isOpen={popoverOpen}
           placement={popType === 'mobile' ? 'left' : 'top'}
-          target={targetId}
-          key={targetId}
-          onBlur={this.toggle}
+          target={triggerId}
+          key={triggerId}
         >
-          <PopoverHeader>
+          <PopoverHeader id={dialogHeaderId}>
             {header}{' '}
             <Button className="close" onClick={this.toggle} aria-label={closeAria}>
               &times;
             </Button>
           </PopoverHeader>
-          <PopoverBody id={describesId}>{popoverText}</PopoverBody>
+          <PopoverBody id={dialogBodyId}>{popoverText}</PopoverBody>
         </Popover>
       </span>
     )
