b1389cdc5c40a51f8c1d2011c9658f33ddc4cb26
Elena Rakhimova <elenara@kth.se>
2020-09-21 12:40:04 +0200

    refactor: desctructure kopps api

---
+++ server/apiCalls/setupKoppsApi.js
@@ -0,0 +1,27 @@
+'use strict'
+const log = require('kth-node-log')
+const config = require('../configuration').server
+const redis = require('kth-node-redis')
+const connections = require('kth-node-api-call').Connections
+
+const koppsOpts = {
+  log,
+  https: true,
+  redis,
+  cache: config.cache,
+  timeout: 5000,
+  defaultTimeout: config.koppsApi.defaultTimeout,
+  retryOnESOCKETTIMEDOUT: true,
+  useApiKey: false // skip key
+}
+
+config.koppsApi.doNotCallPathsEndpoint = true // skip checking _paths, because kopps doesnt have it
+config.koppsApi.connected = true
+
+const koppsConfig = {
+  koppsApi: config.koppsApi
+}
+
+const koppsApi = connections.setup(koppsConfig, koppsConfig, koppsOpts)
+
+module.exports = koppsApi
