fd5e040af9325d306fc1efd2d7aa9efd83b87b45
Elena Rakhimova <elenara@kth.se>
2020-09-21 12:41:47 +0200

    feat: added mocks for transformed for comparison

---
+++ test/mocks/transformedKoppsData.js
@@ -0,0 +1,17 @@
+const transformedKoppsData = {
+  courseCode: 'SF1624',
+  courseTitle: 'Algebra and Geometry',
+  sortedSyllabusStart: ['', 20192, 20102, 20092, 20082],
+  syllabusPeriods: {
+    20082: { endDate: 20091 },
+    20092: { endDate: 20101 },
+    20102: { endDate: 20191 },
+    20192: { endDate: '' }
+  },
+  courseCredits: 7.5,
+  koppsDataLang: 'en',
+  koppsLangIndex: 0
+}
+
+export default transformedKoppsData
+module.exports = transformedKoppsData
