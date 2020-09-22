a097003806ca8ce2cf508ecfef9a72884d3448d6
Elena Rakhimova <elenara@kth.se>
2020-09-22 16:18:39 +0200

    feat: mock for Admin Store

---
+++ test/mocks/mockAdminStore.js
@@ -0,0 +1,18 @@
+import AdminStore from '../../public/js/app/stores/AdminStore'
+import transformedAnalysisData from '../mocks/transformedAnalysisData'
+import transformedKoppsData from '../mocks/transformedKoppsData'
+
+const realAdminStore = new AdminStore()
+
+const mockAdminStore = (userLang = 'en') => {
+  const routerWithData = {
+    ...realAdminStore,
+    courseKoppsData: transformedKoppsData(userLang),
+    analysisData: transformedAnalysisData,
+    browserConfig: { storageUri: '' }
+  }
+
+  return routerWithData
+}
+
+export default mockAdminStore
