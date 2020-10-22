b1a6f8923d2b154b8efeb4b7d25f57e311c3c060
Rickard Falk <rickard.falk@gmail.com>
2020-10-22 08:32:26 +0200

    fix: Import image in app

--- public/js/app/app.jsx
+++ public/js/app/app.jsx
@@ -14,6 +14,7 @@ import StudentViewCourseDev from './pages/StudentViewCourseDev'
 import Archive from './pages/Archive'
 
 import '../../css/kursutveckling-web.scss'
+import '../../img/arrow-black-down.svg'
 
 function appFactory() {
   if (process.env.NODE_ENV !== 'production') {
