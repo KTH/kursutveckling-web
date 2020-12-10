e62beb52ec973cf9d8b2cb90e5abd9959a571556
Elena Rakhimova <elenara@kth.se>
2020-12-10 16:26:35 +0100

    refactor: minor and test fix

--- public/js/app/stores/AdminStore.jsx
+++ public/js/app/stores/AdminStore.jsx
@@ -4,23 +4,24 @@ import axios from 'axios'
 
 const paramRegex = /\/(:[^\/\s]*)/g
 
-function _paramReplace (path, params) {
+function _paramReplace(path, params) {
   let tmpPath = path
   const tmpArray = tmpPath.match(paramRegex)
-  tmpArray.forEach(element => {
+  tmpArray.forEach((element) => {
     tmpPath = tmpPath.replace(element, '/' + params[element.slice(2)])
   })
   return tmpPath
 }
 
-function _webUsesSSL (url) {
+function _webUsesSSL(url) {
   return url.startsWith('https:')
 }
 class AdminStore {
-  @observable courseKoppsData = undefined //kopps
-  analysisData = undefined //kursutveckling-api
-  
-  buildApiUrl (path, params) {
+  @observable courseKoppsData = undefined // kopps
+
+  analysisData = undefined // kursutveckling-api
+
+  buildApiUrl(path, params) {
     let host
     if (typeof window !== 'undefined') {
       host = this.apiHost
@@ -36,7 +37,7 @@ class AdminStore {
     return [host, newPath].join('')
   }
 
-  _getOptions (params) {
+  _getOptions(params) {
     // Pass Cookie header on SSR-calls
     let options
     if (typeof window === 'undefined') {
@@ -44,64 +45,80 @@ class AdminStore {
         headers: {
           Cookie: this.cookieHeader,
           Accept: 'application/json',
-          'X-Forwarded-Proto': (_webUsesSSL(this.apiHost) ? 'https' : 'http')
+          'X-Forwarded-Proto': _webUsesSSL(this.apiHost) ? 'https' : 'http'
         },
         timeout: 10000,
-        params: params
+        params
       }
     } else {
       options = {
-        params: params
+        params
       }
     }
     return options
   }
 
-  @action getLdapUserByUsername (params) {
-    return axios.get(this.buildApiUrl(this.paths.api.searchLdapUser.uri, params), this._getOptions())
-    .then((res) => {
-      return res.data
-    }).catch(err => {
-      if (err.response) {
-        throw new Error(err.message, err.response.data)
-      }
-      throw err
-    })
+  @action getLdapUserByUsername(params) {
+    return axios
+      .get(this.buildApiUrl(this.paths.api.searchLdapUser.uri, params), this._getOptions())
+      .then((res) => {
+        return res.data
+      })
+      .catch((err) => {
+        if (err.response) {
+          throw new Error(err.message, err.response.data)
+        }
+        throw err
+      })
   }
-  @action setUser (userKthId) {
+
+  @action setUser(userKthId) {
     this.user = userKthId
   }
-  @action clearBreadcrumbs () {
+
+  @action clearBreadcrumbs() {
     this.breadcrumbs.replace([])
   }
 
-  @action hasBreadcrumbs () {
+  @action hasBreadcrumbs() {
     return this.breadcrumbs.length > 0
   }
 
-  @action setBrowserConfig (config, paths, apiHost, profileBaseUrl) {
+  @action setBrowserConfig(config, paths, apiHost, profileBaseUrl) {
     this.browserConfig = config
     this.paths = paths
     this.apiHost = apiHost
     this.profileBaseUrl = profileBaseUrl
   }
 
-  @action __SSR__setCookieHeader (cookieHeader) {
+  @action __SSR__setCookieHeader(cookieHeader) {
     if (typeof window === 'undefined') {
       this.cookieHeader = cookieHeader || ''
     }
   }
 
-  @action doSetLanguage (lang) {
+  @action doSetLanguage(lang) {
     this.language = lang
   }
 
-  @action getBrowserInfo () {
-    var navAttrs = ['appCodeName', 'appName', 'appMinorVersion', 'cpuClass',
-      'platform', 'opsProfile', 'userProfile', 'systemLanguage',
-      'userLanguage', 'appVersion', 'userAgent', 'onLine', 'cookieEnabled']
+  @action getBrowserInfo() {
+    var navAttrs = [
+      'appCodeName',
+      'appName',
+      'appMinorVersion',
+      'cpuClass',
+      'platform',
+      'opsProfile',
+      'userProfile',
+      'systemLanguage',
+      'userLanguage',
+      'appVersion',
+      'userAgent',
+      'onLine',
+      'cookieEnabled'
+    ]
     var docAttrs = ['referrer', 'title', 'URL']
-    var value = {document: {}, navigator: {}}
+    var value = { document: {}, navigator: {} }
 
     for (let i = 0; i < navAttrs.length; i++) {
       if (navigator[navAttrs[i]] || navigator[navAttrs[i]] === false) {
@@ -117,11 +134,14 @@ class AdminStore {
     return value
   }
 
-  initializeStore (storeName) {
+  initializeStore(storeName) {
     const store = this
 
-    if (typeof window !== 'undefined' && window.__initialState__ && window.__initialState__[storeName]) {
-
+    if (
+      typeof window !== 'undefined' &&
+      window.__initialState__ &&
+      window.__initialState__[storeName]
+    ) {
       const tmp = JSON.parse(decodeURIComponent(window.__initialState__[storeName]))
       for (let key in tmp) {
         store[key] = tmp[key]
@@ -137,4 +157,3 @@ class AdminStore {
 }
 
 export default AdminStore
-
