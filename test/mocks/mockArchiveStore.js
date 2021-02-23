cfe2a9e59ce1e96c9356ce322cba55b87fa2344e
Carl Frendin <cfrendin@kth.se>
2021-02-23 13:46:39 +0100

    KUI-200 test: update mockdata and tests

--- test/mocks/mockArchiveStore.js
+++ test/mocks/mockArchiveStore.js
@@ -1,10 +1,13 @@
 import ArchiveStore from '../../public/js/app/stores/ArchiveStore'
+import transformedAnalysisData from './transformedAnalysisData'
 
 const realArchiveStore = new ArchiveStore()
 
 const mockArchiveStore = (mockData) => {
   const storeWithData = {
     ...realArchiveStore,
+    analysisData: transformedAnalysisData,
+    browserConfig: { storageUri: '' },
     ...mockData
   }
 
--- test/tests/ArchiveEN.test.js
+++ test/tests/ArchiveEN.test.js
@@ -63,13 +63,13 @@ describe(`User language: ${userLang}. Component <Archive>`, () => {
     expect(allSubHeaders.length).toBe(1)
 
     const allH2Headers = getAllByRole('heading', { level: 2 })
-    expect(allH2Headers.length).toBe(2)
+    expect(allH2Headers.length).toBe(3)
     expect(allH2Headers[0]).toHaveTextContent(translation.label_syllabuses)
     expect(allH2Headers[1]).toHaveTextContent(translation.label_memos)
   })
 
-  test('renders syllabus table', () => {
+  test('renders syllabus, memo and analyses table', () => {
     const syllabusTable = getAllByRole('table')
-    expect(syllabusTable.length).toBe(2)
+    expect(syllabusTable.length).toBe(3)
   })
 })
