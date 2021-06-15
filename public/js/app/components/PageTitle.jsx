055870744323f6742bcddb60c32199a6c7b7ca9d
Elena Rakhimova <elenara@kth.se>
2021-06-15 14:59:02 +0200

    refactor: PageTitle to a function

--- public/js/app/components/PageTitle.jsx
+++ public/js/app/components/PageTitle.jsx
@@ -1,39 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { EMPTY, KURSINFO_ADMIN_URL } from '../util/constants'
 
-class PageTitle extends Component {
-  render() {
-    const title = this.props.courseKoppsData
-    const { pageTitle, translate } = this.props
-    const { courseCode, courseCredits, courseTitle, koppsDataLang } = title
-    const adminPageLink = `${KURSINFO_ADMIN_URL}${courseCode}?l=${koppsDataLang}`
+function PageTitle({ courseKoppsData: titleData, pageTitle, translate }) {
+  const { courseCode, courseCredits, courseTitle, koppsDataLang } = titleData
+  const adminPageLink = `${KURSINFO_ADMIN_URL}${courseCode}?l=${koppsDataLang}`
 
-    const credits =
-      courseCredits !== EMPTY && courseCredits.toString().indexOf('.') < 0
-        ? courseCredits + '.0'
-        : courseCredits
-    const creditUnit =
-      koppsDataLang === 'en' ? `${credits} credits` : `${credits.toString().replace('.', ',')} hp`
+  const credits =
+    courseCredits !== EMPTY && courseCredits.toString().indexOf('.') < 0
+      ? courseCredits + '.0'
+      : courseCredits
+  const creditUnit =
+    koppsDataLang === 'en' ? `${credits} credits` : `${credits.toString().replace('.', ',')} hp`
 
-    const courseName = `${courseCode} ${courseTitle} ${creditUnit}`
-    return (
-      <header role="presentation" className="col">
-        <h1 id="page-heading" aria-labelledby="page-heading page-sub-heading">
-          {pageTitle}
-        </h1>
-        <div id="page-sub-heading-wrapper">
-          <p id="page-sub-heading" aria-hidden="true">
-            {courseCode && courseName}
-          </p>
-          <p id="page-sub-heading-admin-link" className="d-none d-sm-block">
-            <a title={translate.course_admin_title} href={adminPageLink}>
-              {translate.course_admin_title}
-            </a>
-          </p>
-        </div>
-      </header>
-    )
-  }
+  const courseName = `${courseCode} ${courseTitle} ${creditUnit}`
+  return (
+    <header role="presentation" className="col">
+      <h1 id="page-heading" aria-labelledby="page-heading page-sub-heading">
+        {pageTitle}
+      </h1>
+      <div id="page-sub-heading-wrapper">
+        <p id="page-sub-heading" aria-hidden="true">
+          {courseCode && courseName}
+        </p>
+        <p id="page-sub-heading-admin-link" className="d-none d-sm-block">
+          <a title={translate.course_admin_title} href={adminPageLink}>
+            {translate.course_admin_title}
+          </a>
+        </p>
+      </div>
+    </header>
+  )
 }
 
 export default PageTitle
