edf9e8a7fb7e1bfbede77b2e52bb985978273c48
Elena Rakhimova <elenara@kth.se>
2022-03-18 13:08:01 +0100

    clean: some urls in constants

--- public/js/app/util/constants.js
+++ public/js/app/util/constants.js
@@ -1,9 +1,5 @@
-export const PROGRAMME_URL = 'https://www.kth.se/student/kurser/program'
 export const EMPTY = '--no information--'
-export const FORSKARUTB_URL = 'https://www.kth.se/utbildning/forskarutbildning/kurser/org'
 export const COURSE_INFO_URL = '/student/kurser/kurs/'
-export const COURSE_UTVECKLING = '/kursutveckling/'
-export const KTH_SE_URL = '' // TODO www.kth.se
 export const KURSINFO_ADMIN_URL = '/kursinfoadmin/kurser/kurs/'
 export const SYLLABUS_URL = '/student/kurser/kurs/kursplan/'
 export const REGULATED_URL_SV = `https://intra.kth.se/styrning/styrdokument/regler/utbildning-overgripande-1.660834`
