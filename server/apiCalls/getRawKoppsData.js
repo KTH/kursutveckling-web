f24fd81d473e5b0fe660bfc924c81413c1b3604d
Rao-Sabahat-Ali <raosabahat01@live.com>
2022-09-02 16:36:44 +0200

    refactor: Using syllabuses Api instead of courseroundterms

--- server/apiCalls/getRawKoppsData.js
+++ server/apiCalls/getRawKoppsData.js
@@ -4,9 +4,9 @@ const log = require('@kth/log')
 const { server: config } = require('../configuration')
 const api = require('./setupKoppsApi')
 
-const rawKoppsCourseData = async (courseCode) => {
+const rawKoppsCourseData = async (courseCode, lang) => {
   const { client } = api.koppsApi
-  const uri = `${config.koppsApi.basePath}course/${encodeURIComponent(courseCode)}/courseroundterms?fromTerm=20071`
+  const uri = `${config.koppsApi.basePath}syllabuses/${encodeURIComponent(courseCode)}?l=${lang}`
   try {
     const { body: course, statusCode } = await client.getAsync({ uri, useCache: true })
     if (!course || statusCode !== 200) {
