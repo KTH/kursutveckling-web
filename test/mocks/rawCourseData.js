b474b3b823f9518305b2c40dda56b26eb178a992
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2025-05-21 16:21:15 +0200

    feat(KUI-1642): remove unused kopps related logics

--- test/mocks/rawCourseData.js
+++ test/mocks/rawCourseData.js
@@ -1,77 +1,3 @@
-const mockRawKoppsData = {
-  en: {
-    course: {
-      title: 'Algebra and Geometry',
-      courseCode: 'SF1624',
-      departmentCode: 'SF',
-      department: {
-        name: 'SCI/Mathematics',
-        code: 'SF'
-      },
-      credits: 7.5,
-      creditUnitLabel: 'Credits',
-      creditUnitAbbr: 'hp'
-    },
-    publicSyllabusVersions: [
-      {
-        validFromTerm: {
-          term: 20192
-        }
-      },
-      {
-        validFromTerm: {
-          term: 20102
-        }
-      },
-      {
-        validFromTerm: {
-          term: 20092
-        }
-      },
-      {
-        validFromTerm: {
-          term: 20082
-        }
-      }
-    ]
-  },
-  sv: {
-    course: {
-      title: 'Algebra och geometri',
-      courseCode: 'SF1624',
-      departmentCode: 'SF',
-      department: {
-        name: 'SCI/Matematik',
-        code: 'SF'
-      },
-      credits: 7.5,
-      creditUnitLabel: 'Högskolepoäng',
-      creditUnitAbbr: 'hp'
-    },
-    publicSyllabusVersions: [
-      {
-        validFromTerm: {
-          term: 20192
-        }
-      },
-      {
-        validFromTerm: {
-          term: 20102
-        }
-      },
-      {
-        validFromTerm: {
-          term: 20092
-        }
-      },
-      {
-        validFromTerm: {
-          term: 20082
-        }
-      }
-    ]
-  }
-}
 const mockRawLadokData = {
   en: {
     benamning: 'Algebra and Geometry',
@@ -90,6 +16,5 @@ const mockRawLadokData = {
 }
 
 module.exports = {
-  mockRawKoppsData,
   mockRawLadokData
 }
