020915e2fca4b9effd3c5ed1aed361cfe4df798f
axelbjo <axelbjo@kth.se>
2025-08-13 15:09:43 +0200

    refactor(KUI-1939): cleanup

--- server/apiCalls/ladokApi.js
+++ server/apiCalls/ladokApi.js
@@ -7,30 +7,21 @@ const serverConfig = require('../configuration').server
 const client = createApiClient(serverConfig.ladokMellanlagerApi)
 
 async function getLadokCourseData(courseCode, lang) {
-  try {
-    const course = await client.getLatestCourseVersionIncludingCancelled(courseCode, lang)
-    return {
-      courseTitle: course?.benamning ?? '',
-      courseFormattedCredits: course?.omfattning?.formattedWithUnit ?? ''
-    }
-  } catch (error) {
-    throw new Error(`Error fetching course data for ${courseCode}:`, error)
+  const course = await client.getLatestCourseVersionIncludingCancelled(courseCode, lang)
+  return {
+    courseTitle: course?.benamning ?? '',
+    courseFormattedCredits: course?.omfattning?.formattedWithUnit ?? ''
   }
 }
 
 async function getLadokCourseSyllabusPeriodsData(courseCode, lang) {
-  try {
-    const courseSyllabuses = await client.getAllValidCourseSyllabuses(courseCode, lang)
-    const periods = courseSyllabuses.map((x) => x.kursplan.giltigfrom)
-    const uniquePeriods = [...new Set(periods)]
-    const sortedPeriodsInDigits = uniquePeriods.map((x) => transformPeriodInDigits(x)).sort()
-    console.log('sorted periods in digits: ', sortedPeriodsInDigits)
-    const syllabusPeriods = createSyllabusPeriods(sortedPeriodsInDigits)
-    return {
-      syllabusPeriods
-    }
-  } catch (error) {
-    throw new Error(`Error fetching syllabus periods for ${courseCode}:`, error)
+  const courseSyllabuses = await client.getAllValidCourseSyllabuses(courseCode, lang)
+  const periods = courseSyllabuses.map((x) => x.kursplan.giltigfrom)
+  const uniquePeriods = [...new Set(periods)]
+  const sortedPeriodsInDigits = uniquePeriods.map((x) => transformPeriodInDigits(x)).sort()
+  const syllabusPeriods = createSyllabusPeriods(sortedPeriodsInDigits)
+  return {
+    syllabusPeriods
   }
 }
 
